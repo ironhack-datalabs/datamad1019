1e4dd0a6aa1121e25f7a82d2d312fe33d38032a9
Elena de la Fuente <delafuente.elena@gmail.com>
2019-10-25 17:42:40 +0200

    Resultados intermedios

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -1,3 +1,9 @@
+print('probando')
+
+print(2+2)
+
+
+
 #1. Import the NUMPY package under the name np.
 import numpy as np
 
@@ -5,143 +11,112 @@ import numpy as np
 # Two methods to obtain version
 
 print(np.__version__)
-1.16.3
 
 print(np.version.version)
 
 print(np.show_config())
 
-blas_mkl_info:
-  NOT AVAILABLE
-blis_info:
-  NOT AVAILABLE
-openblas_info:
-    libraries = ['openblas', 'openblas']
-    library_dirs = ['/usr/local/lib']
-    language = c
-    define_macros = [('HAVE_CBLAS', None)]
-blas_opt_info:
-    libraries = ['openblas', 'openblas']
-    library_dirs = ['/usr/local/lib']
-    language = c
-    define_macros = [('HAVE_CBLAS', None)]
-lapack_mkl_info:
-  NOT AVAILABLE
-openblas_lapack_info:
-    libraries = ['openblas', 'openblas']
-    library_dirs = ['/usr/local/lib']
-    language = c
-    define_macros = [('HAVE_CBLAS', None)]
-lapack_opt_info:
-    libraries = ['openblas', 'openblas']
-    library_dirs = ['/usr/local/lib']
-    language = c
-    define_macros = [('HAVE_CBLAS', None)]
-NoneTraceback (most recent call last):
-  File "<stdin>", line 1, in <module>
-AttributeError: module 'numpy' has no attribute '_configuration'
->>> print(np.show_config())
-blas_mkl_info:
-  NOT AVAILABLE
-blis_info:
-  NOT AVAILABLE
-openblas_info:
-    libraries = ['openblas', 'openblas']
-    library_dirs = ['/usr/local/lib']
-    language = c
-    define_macros = [('HAVE_CBLAS', None)]
-blas_opt_info:
-    libraries = ['openblas', 'openblas']
-    library_dirs = ['/usr/local/lib']
-    language = c
-    define_macros = [('HAVE_CBLAS', None)]
-lapack_mkl_info:
-  NOT AVAILABLE
-openblas_lapack_info:
-    libraries = ['openblas', 'openblas']
-    library_dirs = ['/usr/local/lib']
-    language = c
-    define_macros = [('HAVE_CBLAS', None)]
-lapack_opt_info:
-    libraries = ['openblas', 'openblas']
-    library_dirs = ['/usr/local/lib']
-    language = c
-    define_macros = [('HAVE_CBLAS', None)]
-None
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
-
+a=np.random.random((2,3,5))
 
 #4. Print a.
-
-
+print(a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
 
+b=np.ones((5,2,3))
+
 
 #6. Print b.
-
-
+print("#6")
+print(b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
+print("#7")
+size_a=a.size
+size_b=b.size
 
-
-
+print(size_a==size_b)
 
 #8. Are you able to add a and b? Why or why not?
-
+print("#8")
+print(a.shape)
+print(b.shape)
+# Not possible since they dont have same shape
 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
-
-
+print("#9")
+c=b.reshape(2,3,5)
+print(c.shape)
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
+print("#10")
+#Both numpy arrays have the same size thus possible to sum
 
-
+d=np.add(a,c)
+print(d)
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
+print("#11")
+print(a)
+print("**")
+print(d)
 
-
-
+#elements of the new array d are the sum of each element of a and b 
 
 #12. Multiply a and c. Assign the result to e.
 
+print("#12")
+e=np.multiply(a,c)
+print(e)
 
 
 #13. Does e equal to a? Why or why not?
-
-
-
+print("#13")
+#They are equal because e is the result of multiplying a which is an array with random numbers and another array with all numbers equal 1. 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
+print("#14")
 
+d_max=np.max(d)
+d_min=np.min(d)
+d_mean=np.mean(d)
+
+print(d_max)
+print(d_min)
+print(d_mean)
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
+print("#15")
+
+f=np.empty((2,3,5))
+print(f)
 
 
-
-
-"""
 #16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
+'''
 If a value in d is larger than d_mean but smaller than d_max, assign 75 to the corresponding value in f.
 If a value equals to d_mean, assign 50 to the corresponding value in f.
 Assign 0 to the corresponding value(s) in f for d_min in d.
 Assign 100 to the corresponding value(s) in f for d_max in d.
 In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
-"""
+'''
+
+print("#16")
 
 
 
 
-"""
+
 #17. Print d and f. Do you have your expected f?
 For instance, if your d is:
 array([[[1.85836099, 1.67064465, 1.62576044, 1.40243961, 1.88454931],
@@ -160,10 +135,9 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
        [[ 25.,  25.,  25.,  25., 100.],
         [ 75.,  75.,  75.,  75.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
-"""
+print("#17")
 
 
-"""
 #18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
 ("A", "B", "C", "D", and "E") to label the array elements? You are expecting the result to be:
 array([[[ 'D',  'D',  'D',  'B',  'D'],
@@ -174,4 +148,4 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'D',  'D',  'D',  'D',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
-"""
+print("#18")
