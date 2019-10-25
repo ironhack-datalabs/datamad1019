0287d7992b94c940f20af0c57cf50903cdc81171
AlexMndzF <alex.93menendez@gmail.com>
2019-10-25 18:31:06 +0200

    Challege 17 acabado

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -1,7 +1,5 @@
 #1. Import the NUMPY package under the name np.
 import numpy as np
-
-
 #2. Print the NUMPY version and the configuration.
 #np.version.version
 print(np.version.version)
@@ -16,47 +14,34 @@ b = np.ones([5,2,3])
 #6. Print b.
 print('Ones b 5x2x3 ----> \n\n',b)
 #7. Do a and b have the same size? How do you prove that in Python code?e
-e = np.reshape(a,(5,2,3))
-
-print('Reshape a 5x2x3----> \n\n',e)
+resiceA = np.reshape(a,(5,2,3))
+print('Reshape a 5x2x3----> \n\n',resiceA)
 #8. Are you able to add a and b? Why or why not?
-addAB = np.add(e,b)
+addAB = np.add(resiceA,b)
 print('add a b ----> \n\n',addAB)
-
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
+c = b.T
 c = np.reshape(b,(2,3,5))
-
-
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 d = np.add(a,c)
 print('add a c ----> \n\n',d)
-
-
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 print('a------>\n\n',a)
 print('d------>\n\n',d)
-
-
-
 #12. Multiply a and c. Assign the result to e.
-
-
-
+e = np.multiply(a,c)
+print('e------>\n\n',e)
 #13. Does e equal to a? Why or why not?
-
-
-
-
+print('e equal a?---------------->')
+print(e == a)
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
-
-
-
-
+d_max = np.max(d)
+d_min = np.min(d)
+d_mean= np.mean(d)
+print('d_max = ',d_max,'\nd_min = ', d_min,'\nd_mean = ', d_mean)
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
-
-
-
-
+f = np.empty([2,3,5],float)
+print('f--------->\n',f)
 """
 #16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
 If a value in d is larger than d_mean but smaller than d_max, assign 75 to the corresponding value in f.
@@ -66,10 +51,24 @@ Assign 100 to the corresponding value(s) in f for d_max in d.
 In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
-
-
-
-
+for a in range(d.shape[0]):
+        for b in range(d.shape[1]):
+                for c in range(d.shape[2]):
+                        if  d_mean < d[a,b,c] < d_max:
+                                f[a,b,c] = 75
+                                continue
+                        elif d_min < d[a,b,c] < d_mean:
+                                f[a,b,c] = 25
+                                continue
+                        elif d[a,b,c] == d_mean:
+                                f[a,b,c] = 50
+                                continue
+                        elif d[a,b,c] == d_min:
+                                f[a,b,c] = 0
+                                continue
+                        elif d[a,b,c] == d_max:
+                                f[a,b,c] = 100
+                                continue
 """
 #17. Print d and f. Do you have your expected f?
 For instance, if your d is:
@@ -90,7 +89,7 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 75.,  75.,  75.,  75.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
-
+print('f modify---------->\n\n',f)
 
 """
 #18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
