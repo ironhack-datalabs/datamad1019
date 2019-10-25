2c5174980ef7f4585d5e49140bc61815d720de39
Ovi <jm.fdez.corbacho@gmail.com>
2019-10-25 15:55:07 +0200

    Añadido hasta f

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -1,67 +1,62 @@
 #1. Import the NUMPY package under the name np.
-
-# Create pull request
+import numpy as np
 
 #2. Print the NUMPY version and the configuration.
-
-
+print(np.version.version)
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
-
-
+a = np.random.random((2,3,5))
+a_1 = np.random.randn(2,3,5)
 
 #4. Print a.
-
-
+print (a)
+# print("\n\n")
+# print (a_1)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
-
-
+b = np.ones((5,2,3))
 
 #6. Print b.
-
-
+print (b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
-
-
-
+print ("¿'a' y 'b' tienen el mismo tamaño? ", a.size == b.size)
 
 #8. Are you able to add a and b? Why or why not?
-
-
+# c = a+b
+# No se pueden sumar dado que aunque tengan el mismo tamaño, para poder operar entre ellas deben tener la misma forma
+# Es tienen que tener el mismo número de elementos en cada dimensión
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
-
-
+c = b.T
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
-
-
+c = np.reshape(c, (2,3,5))
+d = a + c
+# Ahora funciona porque tanto a como c tienen el mismo número de elementos en cada dimensión 2x3x5
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
-
-
-
+print(a)
+print("\n\n")
+print(d)
+# La matriz 'd' tiene mismos elementos de la matriz a+1
 
 #12. Multiply a and c. Assign the result to e.
-
-
+e = a*c
 
 #13. Does e equal to a? Why or why not?
-
-
-
+print("¿Las matrices a y e son iguales? ", e==a)
+#Estamos multiplicando cada elemento x1, por tanto tenemos la misma matriz
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
-
-
-
+d_max = np.max(d)
+d_min = np.min(d)
+d_mean = np.mean(d)
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
-
+f = np.empty((2,3,5))
 
 
 
