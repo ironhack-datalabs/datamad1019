f26a62e480178c7cf495470b8c72c9395896a2a0
jgph91 <jgph91@gmail.com>
2019-10-25 17:26:58 +0200

    v2

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -1,76 +1,81 @@
 #1. Import the NUMPY package under the name np.
 
+print("Ejercicio 1\n")
 import numpy as np
 
 #2. Print the NUMPY version and the configuration.
-
+print("Ejercicio 2\n")
 print(np.version.version)
 
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
-
+print("Ejercicio 3\n")
 a = np.random.random(30).reshape(2,3,5)
 
 #4. Print a.
-
+print("Ejercicio 4\n")
 print(a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
-
+print("Ejercicio 5\n")
 b = np.ones((5,2,3))
 
 #6. Print b.
-
+print("Ejercicio 6\n")
 print(b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
-
+print("Ejercicio 7\n")
 print(a.size == b.size)
 
 #8. Are you able to add a and b? Why or why not?
-
+print("Ejercicio 8\n")
 #np.add(a,b) 
 print("No se pueden sumar, ya que tienen distintos tamaños")
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
-
+print("Ejercicio 9\n")
 c = b.T
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
-
+print("Ejercicio 10\n")
 c = c.reshape(2,3,5)
 d = np.add(a,c)
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
-
+print("Ejercicio 11\n")
 print(a)
 
 print(d)
 
 
 #12. Multiply a and c. Assign the result to e.
-
+print("Ejercicio 12\n")
 e = np.multiply(a,c)
 
 #13. Does e equal to a? Why or why not?
-
+print("Ejercicio 13\n")
+print(e)
+print(a)
 print(np.array_equal(a,e))
 print ("Sí, ya que el el array C son todo unos, luego al hacer a*c el array no varía")
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
-
+print("Ejercicio 14\n")
 d_max = d.max()
 print(d_max)
 d_min = d.min()
 print(d_min)
 d_mean = d.mean()
 print(d_mean)
+
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
-
-
+print("Ejercicio 15\n")
+f = np.empty((2,3,5))
+print(f)
 
 
 """
@@ -82,7 +87,36 @@ Assign 100 to the corresponding value(s) in f for d_max in d.
 In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
+print("Ejercicio 16\n")
 
+valores = []
+for uno in d:
+        for dos in uno:
+                valores.append(dos)
+
+
+
+
+value_list = []
+for values in valores:
+        if (d_min > values) and (values < d_mean):
+                values = 25
+                value_list.append(values)
+        elif (d_mean > values) and (values < d_max):
+                values = 75
+                value_list.append(values)
+        elif  values == d_mean:
+                values = 50
+                value_list.append(values)
+        elif values == d_min:
+                values = 0
+                value_list.append(values)
+        elif values > d_max:
+                values = 100
+                value_list.append(values)
+
+
+        
 
 
 
