51aefefc5077236961ea6c930f333cec7a17c060
Clara <clapiniella@gmail.com>
2019-10-26 14:15:58 +0200

    numpys numpys everywhere

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -1,68 +1,87 @@
 #1. Import the NUMPY package under the name np.
+import numpy as np
 
-Hola Buenas tardes
 
 #2. Print the NUMPY version and the configuration.
+print(np.__version__)
+print(np.show_config())
 
 
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
-
+a = np.random.random((2,3,5))
 
 #4. Print a.
-
+print(a)
 
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
+b= np.ones((5,2,3))
 
 
 
 #6. Print b.
-
+print(b)
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
-
-
+if a.size==b.size:
+        print ("yes, they are")
+else:
+        print(a.size, b.size)
 
 
 #8. Are you able to add a and b? Why or why not?
+if a.shape==b.shape:
+        print("yes")
+else:
+        print("no, cause they have different shapes")
 
 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
-
-
+c= np.reshape(b, (2, 3, 5))
+print(np.shape(c))
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
+d= np.add(a,c)
+print(d)
 
+#Cause now they have the same shape
 
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
+print(a)
+print(d)
 
-
-
+#los decimales son iguales, pero los números enteros son diferentes, porque hemos sumado una matriz de números entre 0-0.99 a una matriz unitaria
 
 #12. Multiply a and c. Assign the result to e.
-
+e= a*c
+print(e)
+print(a)
 
 
 #13. Does e equal to a? Why or why not?
 
-
+#yes, cause we multiply an array with random numbers and a matrix with ones. Any number multiply by one result the number.  
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
-
-
-
+d_max=np.max(d)
+d_min=np.min(d)
+d_mean=np.mean(d)
+print(d_max)
+print(d_min)
+print(d_mean)
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-
+f=np.empty((2,3,5))
+print(f)
 
 
 """
@@ -74,6 +93,21 @@ Assign 100 to the corresponding value(s) in f for d_max in d.
 In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
+for i in range(len(d)):
+        for j in range(len(d[i])):
+                for k in range(len(d[i][j])):
+                        if d[i][j][k]>d_min and d[i][j][k]<d_mean:
+                                f[i][j][k]=25
+                        elif d[i][j][k]>d_mean and d[i][j][k]<d_max:
+                                f[i][j][k]=75
+                        elif d[i][j][k]==d_mean:
+                                f[i][j][k]=50
+                        elif d[i][j][k]==d_min:
+                                f[i][j][k]=0
+                        elif d[i][j][k]==d_max:
+                                f[i][j][k]=100
+
+
 
 
 
@@ -98,8 +132,8 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 75.,  75.,  75.,  75.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
-
-
+print(d)
+print(f)
 """
 #18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
 ("A", "B", "C", "D", and "E") to label the array elements? You are expecting the result to be:
@@ -112,3 +146,20 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
 """
+f=np.empty((2,3,5), dtype=type(str))
+
+for i in range(len(d)):
+        for j in range(len(d[i])):
+                for k in range(len(d[i][j])):
+                        if d[i][j][k]>d_min and d[i][j][k]<d_mean:
+                                f[i][j][k]="A"
+                        elif d[i][j][k]>d_mean and d[i][j][k]<d_max:
+                                f[i][j][k]="B"
+                        elif d[i][j][k]==d_mean:
+                                f[i][j][k]="C"
+                        elif d[i][j][k]==d_min:
+                                f[i][j][k]="D"
+                        elif d[i][j][k]==d_max:
+                                f[i][j][k]="E"
+
+print(f)
