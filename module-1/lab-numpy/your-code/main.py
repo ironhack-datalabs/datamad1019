c128533a3f30eb80a0fe94e80c830679df90548e
seora <seorakwon@gmail.com>
2019-10-26 23:36:37 +0200

    a falta de bonus

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -10,60 +10,74 @@ print(np.show_config())
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
+a = np.random.random((2,3,5))
+
+## Challenge
 
 
 #4. Print a.
 
-
+print(a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
+b = np.ones((5,2,3))
 
 
 #6. Print b.
 
-
+print(b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
-
+print(a.size == b.size)
 
 #8. Are you able to add a and b? Why or why not?
 
-
+## print(np.add(a, b))
+# #No, it is not possible to add a and b as they have to be the same  
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
-
+c = np.reshape(b, (2,3,5))
+print(c)
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-
+d = np.add(a, c)
+print(d)
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
-
-
+print(a)
+print(d)
 
 #12. Multiply a and c. Assign the result to e.
 
-
+e = a*c
+print(e)
 
 #13. Does e equal to a? Why or why not?
 
-
-
+print(a==e)
+# Yes, because..................................
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
+d_max = np.amax(d)
+d_min = np.amin(d)
+d_mean = np.mean(d)
 
+print(d_max)
+print(d_min)
+print(d_mean)
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-
+f = np.empty(shape = (2,3,5))
+print(f)
 
 
 """
@@ -76,7 +90,19 @@ In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
 
-
+for x in range(len(d)):
+        for y in range(len(d[x])):
+                for z in range(len(d[x][y])):
+                        if d[x][y][z] < d_mean and d[x][y][z] > d_min:
+                                f[x][y][z] = 25
+                        elif d[x][y][z] > d_mean and d[x][y][z] < d_max:
+                                f[x][y][z] = 75
+                        elif d[x][y][z] == d_mean:
+                                f[x][y][z] = 50
+                        elif d[x][y][z] == d_min:
+                                f[x][y][z] = 0
+                        elif d[x][y][z] == d_max:
+                                f[x][y][z] = 100
 
 
 """
@@ -99,7 +125,7 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 75.,  75.,  75.,  75.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
-
+print(f)
 
 """
 #18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
