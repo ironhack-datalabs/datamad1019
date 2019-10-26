5914460702174b3b6d7a26b5bdb6adbd9c08ef1d
castares <cesar.castares@gmail.com>
2019-10-26 13:19:19 +0200

    done!

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -1,69 +1,78 @@
 #1. Import the NUMPY package under the name np.
-
-
+import numpy as np
 
 #2. Print the NUMPY version and the configuration.
-
-
+print ("#2")
+print("numpy version is: {}".format(np.__version__))
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
-
+a = np.random.random((2,3,5))
 
 #4. Print a.
-
+print ("#4")
+print(a)
 
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
-
+b = np.ones((5,2,3))
 
 #6. Print b.
 
-
+print ("#6")
+print(b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
+print ("#7")
+print(a.size == b.size)
 
 
 #8. Are you able to add a and b? Why or why not?
 
+print ("#8")
 
+print("No se puede porque a y b tienen diferente forma.")
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
 
+c = b.reshape(a.shape)
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-
+d = a + c
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
+print ("#11")
+
+print("---a--- \n {}\n ---d--- \n {}".format(a,d))
 
 
 
 #12. Multiply a and c. Assign the result to e.
 
-
+e = a * c
 
 #13. Does e equal to a? Why or why not?
-
-
-
+print("#13")
+print(e == a)
+print("[e] es igual a [a] porque es el resultado de multiplicar todos los valores de a por 1")
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
-
-
+d_max = d.max()
+d_min = d.min()
+d_mean = d.mean()
+print("#15\n Max {}, Min {}, Mean {}".format(d_max, d_min, d_mean))
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-
-
+f = np.empty((2,3,5))
 
 """
 #16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
@@ -74,9 +83,29 @@ Assign 100 to the corresponding value(s) in f for d_max in d.
 In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
+print("#16\n")
 
-
-
+lst2 = []
+for value in d:
+        for value2 in value: 
+                for e in value2:               
+                        if (e > d_min) & (e < d_mean):
+                                e = 25
+                                lst2.append(e)
+                        elif (e > d_mean) & (e < d_max):
+                                e = 75
+                                lst2.append(e)
+                        elif e == d_max:
+                                e = 100
+                                lst2.append(e)
+                        elif e == d_min:
+                                e = 0
+                                lst2.append(e)
+                        elif e == d_mean:
+                                e = 50
+                                lst2.append(e)
+lst2 = np.asarray(lst2)
+f = lst2.reshape((2,3,5))
 
 """
 #17. Print d and f. Do you have your expected f?
@@ -98,7 +127,7 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 75.,  75.,  75.,  75.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
-
+print("#17 d:\n{}\n f:\n{}".format(d, f))
 
 """
 #18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
@@ -112,3 +141,27 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
 """
+print("#18")
+lst2 = []
+for value in d:
+        for value2 in value: 
+                for e in value2:               
+                        if (e > d_min) & (e < d_mean):
+                                e = "B"
+                                lst2.append(e)
+                        elif (e > d_mean) & (e < d_max):
+                                e = "C"
+                                lst2.append(e)
+                        elif e == d_max:
+                                e = "E"
+                                lst2.append(e)
+                        elif e == d_min:
+                                e = "A"
+                                lst2.append(e)
+                        elif e == d_mean:
+                                e = "D"
+                                lst2.append(e)
+lst2 = np.asarray(lst2)
+f = lst2.reshape((2,3,5))
+
+print(f)
