140421c5f04f0b8d3162767dcd7d8982e9100c45
albertovpd <a.vargas.pina@gmail.com>
2019-10-26 13:53:07 +0200

    hasta el 18

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -101,37 +101,50 @@ print("mean",d_mean)
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
-print("\n"+"==============="+"\n"+"ej15: label the values")
+print("\n"+"==============="+"\n"+"ej15: numpy vacío")
+print("si uso f=np.empty((2,3,5)) lo que obtengo es un numpy NO vacío. No sé por qué, mira:")
 f=np.empty((2,3,5))
+print(f)
 print(np.shape(f))
-
-
+print("Peeeero si uso g=numpy.array([])) sí que consigo un numpy vacío")
+g=np.array([])
+print("mi numpy vacío es ", g)
+print(np.shape(g))
 
 """
 #16. Populate the values in f. For each value in d, if it's larger than
 #  d_min but smaller than d_mean, assign 25 to the corresponding value in f.
+
 If a value in d is larger than d_mean but smaller than d_max, assign 75 to 
 the corresponding value in f.
+
 If a value equals to d_mean, assign 50 to the corresponding value in f.
-Assign 0 to the corresponding value(s) in f for d_min in d.
+
+Assign 0 to the corresponding value(s) in f for d_min in d. 
+
 Assign 100 to the corresponding value(s) in f for d_max in d.
 In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 
 """
-print("\n"+"==============="+"\n"+"ej16: comparar")
+print("\n"+"==============="+"\n"+"ej16: llenar lista vacía con parámetros")
 print("")
 
-#[np.append([f],[25]) for e in np.nditer(d) if e>d_min and if e<d_mean]
-#for e in np.nditer(d):
- #       if e>d_min(d) and e<d_mean(d):
-  #              f[x]=25
-   #     else e>d_mean(d) and e<d_max(d):
-     #            f[x]=
-      #  else 
-
-print(f)
-for e in 
+for i in range(len(d)):
+        for j in range(len(d[i])):
+                for k in range(len(d[i][j])):
+                        if d[i][j][k]>d_min and d[i][j][k]<d_mean:
+                                f[i,j,k]=25
+                        elif d[i][j][k]>d_mean and d[i][j][k]<d_max:
+                                f[i,j,k]=75
+                        elif d[i][j][k]==d_mean:
+                                f[i,j,k]=50
+                        elif d[i][j][k]==d_min:
+                                f[i][j][k]=0
+                        elif d[i][j][k]==d_max:
+                                f[i][j][k]==100
+                        
+                        
 
 """
 #17. Print d and f. Do you have your expected f?
@@ -153,8 +166,11 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 75.,  75.,  75.,  75.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
-print("\n"+"==============="+"\n"+"ej17: comparar")
-print("")
+print("\n"+"==============="+"\n"+"ej17: mi función f")
+print(d,"\n",f)
+
+print("tengo un error que intentaré mirar si tengo tiempo")
+
 
 """
 #18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
@@ -168,5 +184,34 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
 """
-print("\n"+"==============="+"\n"+"ej18: comparar")
+print("\n"+"==============="+"\n"+"ej18: ")
 print("")
+
+
+
+nueva=np.empty((2,3,5))
+print(nueva)
+
+f=f.astype(str)
+for i in range(len(f)):
+        for j in range(len(f[i])):
+                for k in range(len(f[i][j])):
+                        if f[i,j,k]==25:
+                                f[i,j,k]="A"
+                                '''
+                        elif f[i,j,k]==75:
+                                f[i,j,k]="B"
+                        elif f[i,j,k]==50:
+                                f[i,j,k]="C"
+                        elif f[i][j][k]==0:
+                                f[i,j,k]="D"
+                        elif f[i][j][k]==100:
+                                f[i,j,k]="E"
+                        '''
+
+
+print(f)
+
+            
+
+            
