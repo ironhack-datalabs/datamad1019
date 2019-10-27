768252550f54284a2360f3b66887cb1b3d8dff4b
mariaadradosr <mariaadradosr@gmail.com>
2019-10-27 17:28:47 +0100

    Cuasi resolución kata weird matrix

--- module-1/lab-numpy/your-code/katas.py
+++ module-1/lab-numpy/your-code/katas.py
@@ -9,3 +9,23 @@ def color_probability(color, texture):
     return str(balls[texture].count(color)/len(balls[texture]))[:4]
 
 print(color_probability('red', 'smooth'))
+
+
+# Weird matrix multiplication
+
+# La función que he planteado pasa todos los tests (example cases y edge cases)
+# excepto el stress test :( 
+# lo he intentado 150 millones de veces y ya no sé cómo corregir
+# el código
+
+import numpy as np
+
+def weird_mul(A, B):
+    c = []
+    if (B.shape > (1, 0)) and (A.shape > (1, 0)) and (len(B.shape) ==2) and (len(A.shape)==2):
+        for i in range(A.shape[0]):
+            for k in range(B.shape[0]):
+                for j in range(A.shape[1]):
+                    c += [A[i][j]*x for x in B[k]]
+        return np.array(c).reshape(A.shape[0]*B.shape[0], A.shape[1]*B.shape[1])
+    return None
