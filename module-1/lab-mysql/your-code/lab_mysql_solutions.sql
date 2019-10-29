0bb483ef46fcd19d8693f38136860c04bee14eab
alfpml <alfpml@gmail.com>
2019-10-29 23:21:33 +0100

    Challenge 3

--- module-1/lab-mysql/your-code/lab_mysql_solutions.sql
+++ module-1/lab-mysql/your-code/lab_mysql_solutions.sql
@@ -105,3 +105,42 @@ DEFAULT CHARACTER SET = latin1;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+---Challenge 3: Seeding.sql
+
+INSERT INTO Cars
+(ID,VIN,Manufacturer,Model,Year,Color)
+VALUES
+(0,'3K096I98581DHSNUP','Volkswagen','Tiguan',2019,'Blue'),
+(1,'ZM8G7BEUQZ97IH46V','Peugeot','Rifter',2019,'Red'),
+(2,'RKXVNNIHLVVZOUB4M','Ford','Fusion',2018,'White'),
+(3,'HKNDGS7CU31E9Z7JW','Toyota','RAV4',2018,'Silver'),
+(4,'DAM41UDN3CHU2WVF6','Volvo','V60',2019,'Gray'),
+(5,'DAM41UDN3CHU2WVF6','Volvo','V60 Cross Country',2019,'Gray')
+;
+INSERT INTO Customers
+(ID,Customer_ID,Name,Phone,Email,Address,City,State_Province,Country,Postal)
+VALUES
+(0,10001,'Pablo Picasso',34636176382,'Paseo de la Chopera, 14','Madrid','Madrid','Spain',28045),
+(1,20001,'Abraham Lincoln',13059077086,'120 SW 8th St','Miami','Florida','United States',33130),
+(2,30001,'Napoleon Bonaparte',33179754000,'40 Rue du Colisée','Paris','Île-de-France','France',75008)
+;
+INSERT INTO Invoices
+(ID,Invoice_Number,Date,Car,Customer,Staff)
+VALUES
+(0,852399038,'2018-08-22',0,1,3),
+(1,731166526,'2018-12-31',3,0,5),
+(2,271135104,'2019-01-22',2,2,7)
+;
+INSERT INTO Sales_Persons
+(ID,Staff_ID,Name,Store)
+VALUES
+(0,1,'Petey Cruiser','Madrid'),
+(1,2,'Anna Sthesia','Barcelona'),
+(2,3,'Paul Molive','Berlin'),
+(3,4,'Gail Forcewind','Paris'),
+(4,5,'Paige Turner','Miami'),
+(5,6,'Bob Frapples','Mexico City'),
+(6,7,'Walter Melon','Amsterdam'),
+(7,8,'Shonda Leer','São Paulo')
+;
