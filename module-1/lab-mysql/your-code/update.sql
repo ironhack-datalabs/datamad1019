83efe4c67f7b5784e88e89ea0794db83dd613216
Cristina Rota <cristina.rota.m@gmail.com>
2019-10-29 18:46:47 +0100

    Adding update.sql

---
+++ module-1/lab-mysql/your-code/update.sql
@@ -0,0 +1,8 @@
+
+# SELECT * FROM mydb.salespersons;
+UPDATE `mydb`.`salespersons` SET `Store` = 'Miami' WHERE (`ID` = '4');
+
+# SELECT * FROM mydb.customers;
+UPDATE `mydb`.`customers` SET `Email` = 'ppicasso@gmail.com' WHERE (`ID` = '0');
+UPDATE `mydb`.`customers` SET `Email` = 'lincoln@us.gov' WHERE (`ID` = '1');
+UPDATE `mydb`.`customers` SET `Email` = 'hello@napoleon.me' WHERE (`ID` = '2');
