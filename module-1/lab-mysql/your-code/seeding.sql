f5ee2939f9309318fd9fc408b3f3d595fec198ff
blancaalcala <blancaalcala97@gmail.com>
2019-10-29 18:37:19 +0100

    bonus final

--- module-1/lab-mysql/your-code/seeding.sql
+++ module-1/lab-mysql/your-code/seeding.sql
@@ -3,6 +3,7 @@
 INSERT INTO `car` (idcars, VIN, manufacturer, model, year, color) VALUES (0, '3K096I98581DHSNUP', 'Volkswagen', 'Tiguan', 2019, 'Blue');
 INSERT INTO `car` (idcars, VIN, manufacturer, model, year, color) VALUES (1, 'ZM8G7BEUQZ97IH46V', 'Peugeot', 'Rifter', 2019, 'Red');
 INSERT INTO `car` (idcars, VIN, manufacturer, model, year, color) VALUES (2, 'RKXVNNIHLVVZOUB4M', 'Ford', 'Fusion', 2018, 'White');
+INSERT INTO `car` (idcars, VIN, manufacturer, model, year, color) VALUES (3, 'HKNDGS7CU31E9Z7JW	', 'Toyota', 'RAV4', 2018, 'Silver');
 INSERT INTO `car` (idcars, VIN, manufacturer, model, year, color) VALUES (4, 'DAM41UDN3CHU2WVF6', 'Volvo', 'V60', 2019, 'Gray');
 INSERT INTO `car` (idcars, VIN, manufacturer, model, year, color) VALUES (5, 'DAM41UDN3CHU2WVF6', 'Volvo', 'V60 Cross Country', 2019, 'Gray');
 	# introduce elements CUSTOMER TABLE
