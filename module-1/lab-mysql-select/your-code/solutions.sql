cf84595669d56d0801a8a5586d27560884f5c18b
jgph91 <jgph91@gmail.com>
2019-10-29 21:45:25 +0100

    v2

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -8,3 +8,19 @@ INNER JOIN publishers AS p ON p.pub_id = t.pub_id
 
 
 #challenge 2
+
+SELECT au_lname, count(title)
+FROM authors AS a
+INNER JOIN titleauthor AS ta ON a.au_id = ta.au_id
+INNER JOIN titles AS t ON t.title_id = ta.title_id
+INNER JOIN publishers AS p ON p.pub_id = t.pub_id 
+GROUP BY au_lname
+
+#challenge 3
+
+SELECT au_fname,qty
+FROM authors AS a
+INNER JOIN titleauthor AS ta ON a.au_id = ta.au_id
+INNER JOIN titles AS t ON t.title_id = ta.title_id
+INNER JOIN sales as s ON s.title_id = ta.title_id
+ORDER BY qty desc
