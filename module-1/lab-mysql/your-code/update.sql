d03f5f96930df2f0688e613b868d4cf460bbae22
María <verdejosinmaria@gmail.com>
2019-10-29 21:48:51 +0100

    primer lab acabado

---
+++ module-1/lab-mysql/your-code/update.sql
@@ -0,0 +1,3 @@
+UPDATE `ejerciciosql`.`customer` SET `email` = 'lincoln@us.gov' WHERE (`ID` = '1');
+UPDATE `ejerciciosql`.`customer` SET `email` = 'hello@napoleon.me' WHERE (`ID` = '2');
+UPDATE `ejerciciosql`.`customer` SET `email` = '	ppicasso@gmail.com' WHERE (`ID` = '0');
