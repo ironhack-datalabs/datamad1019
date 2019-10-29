f814852849bb6f5c1a6ed955d87b2343e816bedd
Ovi <jm.fdez.corbacho@gmail.com>
2019-10-29 23:22:12 +0100

    Añadido challenge 2

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -1,7 +1,23 @@
 # Challenge 1
 SELECT
 	au.au_id AS `AUTHOR ID`, au.au_lname AS `LAST NAME`, au.au_fname AS `FIRST NAME`, t.title AS `TITLE`, p.pub_name AS `PUBLISHER`
-    FROM `lab-select`.authors AS au
+    FROM authors AS au
     INNER JOIN titleauthor AS ta ON au.au_id=ta.au_id
     INNER JOIN titles AS t ON ta.title_id=t.title_id
     INNER JOIN publishers AS p ON t.pub_id=p.pub_id
+
+# Challenge 2
+SELECT
+	au.au_id AS `AUTHOR ID`, au.au_lname AS `LAST NAME`, au.au_fname AS `FIRST NAME`, p.pub_name AS `PUBLISHER`, COUNT(*) AS `TITLE COUNT`
+    FROM authors AS au
+    INNER JOIN titleauthor AS ta ON au.au_id=ta.au_id
+    INNER JOIN titles AS t ON ta.title_id=t.title_id
+    INNER JOIN publishers AS p ON t.pub_id=p.pub_id
+    GROUP BY t.pub_id, au.au_id
+
+
+SELECT
+	au.au_id AS `AUTHOR ID`, au.au_lname AS `LAST NAME`, au.au_fname AS `FIRST NAME`, SUM(s.qty) AS `TOTAL`
+    FROM authors AS au
+    INNER JOIN titleauthor AS ta ON au.au_id = ta.au_id
+    INNER JOIN sales AS s ON ta.title_id = s.title_id
