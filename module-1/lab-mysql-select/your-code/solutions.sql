d85e0d12d9fc00f376979acfe6e3223ff343eb32
mariobru <mariobrubarrero@gmail.com>
2019-10-30 00:03:01 +0100

    Challenge 2 done

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -7,3 +7,18 @@ on ta.title_id = t.title_id
 inner join publications.publishers p
 on t.pub_id = p.pub_id
 order by AUTHOR_ID;
+
+select a.au_id as AUTHOR_ID, a.au_lname as LAST_NAME, a.au_fname as FIRST_NAME, p.pub_name as PUBLISHER, count(*) as TITLE_COUNT
+from publications.authors a
+inner join publications.titleauthor ta
+on a.au_id = ta.au_id
+inner join publications.titles t
+on ta.title_id = t.title_id
+inner join publications.publishers p
+on t.pub_id = p.pub_id
+group by t.title_id
+order by AUTHOR_ID desc;
+
+
+
+
