1c0a4ebc900981910a559fe7d7781613275438c1
Celia Prieto <cprietosegura@gmail.com>
2019-10-29 18:51:02 +0100

    terminado el create, añadido el png con el esquema, en proceso con seeding

--- module-1/lab-mysql/create.sql
+++ module-1/lab-mysql/create.sql
@@ -1,1 +1,96 @@
-para hacer la pull request.
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema lab-mysql
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema lab-mysql
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `lab-mysql` DEFAULT CHARACTER SET utf8 ;
+USE `lab-mysql` ;
+
+-- -----------------------------------------------------
+-- Table `lab-mysql`.`Cars`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `lab-mysql`.`Cars` (
+  `idCars` INT NOT NULL,
+  `VIN` CHAR(17) NULL,
+  `Manufacturer` VARCHAR(25) NULL,
+  `Model` VARCHAR(25) NULL,
+  `Year` YEAR NULL,
+  `Color` VARCHAR(20) NULL,
+  PRIMARY KEY (`idCars`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `lab-mysql`.`Customers`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `lab-mysql`.`Customers` (
+  `id` INT NOT NULL,
+  `Customer ID` CHAR(5) NULL,
+  `Name` VARCHAR(30) NULL,
+  `Phone` VARCHAR(20) NULL,
+  `Email` VARCHAR(45) NULL,
+  `Address` VARCHAR(50) NULL,
+  `City` VARCHAR(20) NULL,
+  `State/Province` VARCHAR(20) NULL,
+  `Country` VARCHAR(20) NULL,
+  `Postal` CHAR(5) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `lab-mysql`.`Salespersons`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `lab-mysql`.`Salespersons` (
+  `idSalespersons` INT NOT NULL,
+  `Staff ID` CHAR(5) NULL,
+  `Name` VARCHAR(20) NULL,
+  `Store` VARCHAR(15) NULL,
+  PRIMARY KEY (`idSalespersons`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `lab-mysql`.`Invoices`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `lab-mysql`.`Invoices` (
+  `ID` INT NOT NULL,
+  `Invoice Number` CHAR(9) NULL,
+  `Date` DATE NULL,
+  `Car` INT NOT NULL,
+  `Customers_id` INT NOT NULL,
+  `Sales persons` INT NOT NULL,
+  PRIMARY KEY (`ID`),
+  INDEX `fk_Invoices_Salespersons1_idx` (`Sales persons` ASC),
+  INDEX `fk_Invoices_Cars1_idx` (`Car` ASC),
+  INDEX `fk_Invoices_Customers1_idx` (`Customers_id` ASC),
+  CONSTRAINT `fk_Invoices_Salespersons1`
+    FOREIGN KEY (`Sales persons`)
+    REFERENCES `lab-mysql`.`Salespersons` (`idSalespersons`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Invoices_Cars1`
+    FOREIGN KEY (`Car`)
+    REFERENCES `lab-mysql`.`Cars` (`idCars`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Invoices_Customers1`
+    FOREIGN KEY (`Customers_id`)
+    REFERENCES `lab-mysql`.`Customers` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
---
+++ module-1/lab-mysql/seeding.sql
@@ -0,0 +1,21 @@
+INSERT INTO Cars () VALUES(0,'3K096I98581DHSNUP','Volkswagen','Tiguan',2019,'Blue');
+INSERT INTO Cars () VALUES(1,'ZM8G7BEUQZ97IH46V', 'Peugeot','Rifter',2019,'Red');
+INSERT INTO Cars () VALUES(2,'RKXVNNIHLVVZOUB4M','Ford','Fusion',2018,'White');
+INSERT INTO Cars () VALUES(3,'HKNDGS7CU31E9Z7JW','Toyota','RAV4',2018,'Silver');
+INSERT INTO Cars () VALUES(4,'DAM41UDN3CHU2WVF6','Volvo','V60',2019,'Gray');
+INSERT INTO Cars () VALUES(5,'DAM41UDN3CHU2WVF6','Volvo','V60 Cross Country',2019,'Gray')
+
+SELECT * FROM Cars;
+
+
+INSERT INTO Customers () VALUES(0,10001,'Pablo Picasso','+34 636 17 63 82','-','Paseo de la Chopera, 14','Madrid','Madrid','Spain',28045);
+INSERT INTO Customers () VALUES(1,20001,'Abraham Lincoln','+1 305 907 7086','-','120 SW 8th St','Miami','Florida','United States',33130);
+INSERT INTO Customers () VALUES(2,30001,'Napoléon Bonaparte','+33 1 79 75 40 00','-','40 Rue du Colisée'
+Paris
+Île-de-France
+France
+75008
+
+
+
+SELECT * FROM Customers;
