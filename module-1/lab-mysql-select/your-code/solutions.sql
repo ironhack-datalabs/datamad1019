3db7e526c4cab9abdc46353d83891d74c6cf3bd8
Ovi <jm.fdez.corbacho@gmail.com>
2019-10-29 20:23:04 +0100

    Descanso

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -1,6 +1,7 @@
+# Challenge 1
 SELECT
 	au.au_id AS `AUTHOR ID`, au.au_lname AS `LAST NAME`, au.au_fname AS `FIRST NAME`, t.title AS `TITLE`, p.pub_name AS `PUBLISHER`
     FROM `lab-select`.authors AS au
-    LEFT JOIN titleauthor AS ta ON au.au_id=ta.au_id
-    LEFT JOIN titles AS t ON ta.title_id=t.title_id
-    LEFT JOIN publishers AS p ON t.pub_id=p.pub_id
+    INNER JOIN titleauthor AS ta ON au.au_id=ta.au_id
+    INNER JOIN titles AS t ON ta.title_id=t.title_id
+    INNER JOIN publishers AS p ON t.pub_id=p.pub_id
