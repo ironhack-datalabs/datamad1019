e1a46a98027519b46ac372892878283e982f3cfe
mariobru <mariobrubarrero@gmail.com>
2019-10-29 17:57:17 +0100

    Challenge 2 done.

---
+++ module-1/lab-mysql/your-code/challenge2.sql
@@ -0,0 +1,96 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema lab_mysql
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema lab_mysql
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `lab_mysql` DEFAULT CHARACTER SET utf8 ;
+USE `lab_mysql` ;
+
+-- -----------------------------------------------------
+-- Table `lab_mysql`.`cars`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `lab_mysql`.`cars` (
+  `id` INT NOT NULL,
+  `vin` INT NULL,
+  `manufacturer` VARCHAR(45) NULL,
+  `model` VARCHAR(45) NULL,
+  `year` INT NULL,
+  `color` VARCHAR(45) NULL,
+  `carscol` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `lab_mysql`.`customers`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `lab_mysql`.`customers` (
+  `id` INT NOT NULL,
+  `customer_id` INT NULL,
+  `name` VARCHAR(45) NULL,
+  `phone` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NULL,
+  `address` VARCHAR(45) NULL,
+  `city` VARCHAR(45) NULL,
+  `state_province` VARCHAR(45) NULL,
+  `country` VARCHAR(45) NULL,
+  `postal` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `lab_mysql`.`salespersons`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `lab_mysql`.`salespersons` (
+  `id` INT NOT NULL,
+  `staff_id` INT NULL,
+  `name` VARCHAR(45) NULL,
+  `store` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `lab_mysql`.`invoices`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `lab_mysql`.`invoices` (
+  `id` INT NOT NULL,
+  `invoicescol` VARCHAR(45) NULL,
+  `date` DATE NULL,
+  `car` INT NOT NULL,
+  `customer` INT NOT NULL,
+  `salesperson` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_invoices_customers_idx` (`customer` ASC),
+  INDEX `fk_invoices_salespersons1_idx` (`salesperson` ASC),
+  INDEX `fk_invoices_cars1_idx` (`car` ASC),
+  CONSTRAINT `fk_invoices_customers`
+    FOREIGN KEY (`customer`)
+    REFERENCES `lab_mysql`.`customers` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_invoices_salespersons1`
+    FOREIGN KEY (`salesperson`)
+    REFERENCES `lab_mysql`.`salespersons` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_invoices_cars1`
+    FOREIGN KEY (`car`)
+    REFERENCES `lab_mysql`.`cars` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
