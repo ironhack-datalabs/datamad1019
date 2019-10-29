52e206b18f0f5aecb790845c86c89ab37320d695
blancaalcala <blancaalcala97@gmail.com>
2019-10-29 18:36:29 +0100

    bonus

---
+++ module-1/lab-mysql/your-code/delete.sql
@@ -0,0 +1,1 @@
+DELETE FROM car WHERE idcars = '4'
---
+++ module-1/lab-mysql/your-code/update.sql
@@ -0,0 +1,5 @@
+SET SQL_SAFE_UPDATES = 0;
+UPDATE salesperson SET store = "Miami" WHERE name = "Paige Turner";
+UPDATE `customer` SET `email` = 'ppicasso@gmail.com' WHERE (`id` = '0');
+UPDATE `customer` SET `email` = 'lincoln@us.gov' WHERE (`id` = '1');
+UPDATE `customer` SET `email` = 'hello@napoleon.me' WHERE (`id` = '2');
