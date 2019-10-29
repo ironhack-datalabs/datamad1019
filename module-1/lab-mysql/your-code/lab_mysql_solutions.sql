50ccab2ee3a3dd2dfb28e3bc24b80de8fc6b0d65
alfpml <alfpml@gmail.com>
2019-10-29 17:16:54 +0100

    initial commit

---
+++ module-1/lab-mysql/your-code/lab_mysql_solutions.sql
@@ -0,0 +1,10 @@
+--Table: Cars
+CREATE TABLE `Lab_MySQL`.`Cars` (
+  `Car_ID` INT NOT NULL,
+  `VIN` VARCHAR(45) NOT NULL,
+  `Manufacturer` VARCHAR(45) NOT NULL,
+  `Model` VARCHAR(45) NOT NULL,
+  `Year` INT NOT NULL,
+  PRIMARY KEY (`Car_ID`));
+
+  --Table 
