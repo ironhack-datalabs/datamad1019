906700f1db04fcadd4daed6f4a68d5c20f92f9ed
Joaquin <jperezdeayala@gmail.com>
2019-10-29 19:37:12 +0100

    seeding

---
+++ module-1/lab-mysql/seeding.sql
@@ -0,0 +1,18 @@
+INSERT INTO `Carsbueno`.`Cars` (`idCars`, `VIN`, `Manufacturer`, `Model`, `Year`, `Color`) VALUES ('0', '3K096I98581DHSNUP', 'Volkswagen', 'Tiguan', '2019', 'Blue');
+INSERT INTO `Carsbueno`.`Cars` (`idCars`, `VIN`, `Manufacturer`, `Model`, `Year`, `Color`) VALUES ('1', 'ZM8G7BEUQZ97IH46V', 'Peugeot', 'Rifter', '2019', 'Red');
+INSERT INTO `Carsbueno`.`Cars` (`idCars`, `VIN`, `Manufacturer`, `Model`, `Year`, `Color`) VALUES ('2', 'RKXVNNIHLVVZOUB4M', 'Ford', 'Fusion', '2018', 'White');
+INSERT INTO `Carsbueno`.`Cars` (`idCars`, `VIN`, `Manufacturer`, `Model`, `Year`, `Color`) VALUES ('3', 'HKNDGS7CU31E9Z7JW', 'Toyota', 'RAV4', '2018', 'Silver');
+INSERT INTO `Carsbueno`.`Cars` (`idCars`, `VIN`, `Manufacturer`, `Model`, `Year`, `Color`) VALUES ('4', 'DAM41UDN3CHU2WVF6', 'Volvo', 'V60', '2019', 'Gray');
+INSERT INTO `Carsbueno`.`Cars` (`idCars`, `VIN`, `Manufacturer`, `Model`, `Year`, `Color`) VALUES ('5', 'DAM41UDN3CHU2WVF6', 'Volvo', 'V60 Cross Country', '2019', 'Gray');
+INSERT INTO `Carsbueno`.`Customers` (`idCustomers`, `Customer ID`, `Name`, `Phone`, `Email`, `Address`, `City`, `State/Province`, `Country`, `Postal`) VALUES ('10001', '10001', 'Pablo Picasso', '+34 636 17 63 82', '-', 'Paseo de la Chopera, 14', 'Madrid', 'Madrid', 'Spain', '28045');
+INSERT INTO `Carsbueno`.`Customers` (`idCustomers`, `Customer ID`, `Name`, `Phone`, `Email`, `Address`, `City`, `State/Province`, `Country`, `Postal`) VALUES ('20001', '20001', 'Abraham Lincoln', '+1 305 907 7086', '-', '120 SW 8th St', 'Miami', 'Florida', 'United States', '33130');
+INSERT INTO `Carsbueno`.`Customers` (`idCustomers`, `Customer ID`, `Name`, `Phone`, `Email`, `Address`, `City`, `State/Province`, `Country`, `Postal`) VALUES ('30001', '30001', 'Napoleón Bonaparte', '+33 1 79 75 40 00', '-', '40 Rue du Colisée', 'Paris', 'Île-de-France', 'France', '75008');
+INSERT INTO `Carsbueno`.`SalesPerson` (`idSalesPerson`, `Staff ID`, `Name`, `Store`) VALUES ('0', '00001', 'Petey Cruiser', 'Madrid');
+INSERT INTO `Carsbueno`.`SalesPerson` (`idSalesPerson`, `Staff ID`, `Name`, `Store`) VALUES ('1', '00002', 'Anna Sthesia', 'Barcelona');
+INSERT INTO `Carsbueno`.`SalesPerson` (`idSalesPerson`, `Staff ID`, `Name`, `Store`) VALUES ('2', '00003', 'Paul Molive', 'Berlin');
+INSERT INTO `Carsbueno`.`SalesPerson` (`idSalesPerson`, `Staff ID`, `Name`, `Store`) VALUES ('3', '00004', 'Gail Forcewind', 'Paris');
+INSERT INTO `Carsbueno`.`SalesPerson` (`idSalesPerson`, `Staff ID`, `Name`, `Store`) VALUES ('4', '00005', 'Paige Turner', 'Miami');
+INSERT INTO `Carsbueno`.`SalesPerson` (`idSalesPerson`, `Staff ID`, `Name`, `Store`) VALUES ('5', '00006', 'Bob Frapples', 'Mexico City');
+INSERT INTO `Carsbueno`.`SalesPerson` (`idSalesPerson`, `Staff ID`, `Name`, `Store`) VALUES ('6', '00007', 'Walter Melon', 'Amsterdam');
+INSERT INTO `Carsbueno`.`SalesPerson` (`idSalesPerson`, `Staff ID`, `Name`, `Store`) VALUES ('7', '00008', 'Shonda Leer', 'São Paulo');
+# para añadir los invoices me está dando un error que no sé cual es, he estado buscando por internet pero las soluciones que me dan no me ayudan
