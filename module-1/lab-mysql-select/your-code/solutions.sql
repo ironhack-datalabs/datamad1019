db2d0424702a231eae0a81dd7780153a6ed9a3b9
Ovi <jm.fdez.corbacho@gmail.com>
2019-10-29 20:02:48 +0100

    Añadido challenge 1

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -1,1 +1,6 @@
-#Creandopull
+SELECT
+	au.au_id AS `AUTHOR ID`, au.au_lname AS `LAST NAME`, au.au_fname AS `FIRST NAME`, t.title AS `TITLE`, p.pub_name AS `PUBLISHER`
+    FROM `lab-select`.authors AS au
+    LEFT JOIN titleauthor AS ta ON au.au_id=ta.au_id
+    LEFT JOIN titles AS t ON ta.title_id=t.title_id
+    LEFT JOIN publishers AS p ON t.pub_id=p.pub_id
