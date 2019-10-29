dabeaa7ca68238c6b2e2770402e91a26a7ce3112
Joaquin <jperezdeayala@gmail.com>
2019-10-29 19:51:07 +0100

    bonus

---
+++ module-1/lab-mysql/update.sql
@@ -0,0 +1,3 @@
+UPDATE `Carsbueno`.`Customers` SET `Email` = 'ppicasso@gmail.com' WHERE (`idCustomers` = '0');
+UPDATE `Carsbueno`.`Customers` SET `Email` = 'lincoln@us.gov' WHERE (`idCustomers` = '1');
+UPDATE `Carsbueno`.`Customers` SET `Email` = 'hello@napoleon.me' WHERE (`idCustomers` = '2');
