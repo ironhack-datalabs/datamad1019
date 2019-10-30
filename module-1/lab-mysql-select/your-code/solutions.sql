d3200924d2eff7a96d97e9e9996e92e801e129d1
MaikaRG <mcrodgar22@gmail.com>
2019-10-30 10:13:51 +0100

    ordenar 1challenge,mas beautifull

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -4,7 +4,8 @@ SELECT publications.authors.au_id,publications.authors.au_lname,
         publications.publishers.pub_name FROM publications.authors
         INNER JOIN publications.titleauthor ON publications.authors.au_id = publications.titleauthor.au_id
         INNER JOIN publications.titles ON publications.titles.title_id = publications.titleauthor.title_id
-        INNER JOIN publications.publishers ON publications.titles.pub_id = publications.publishers.pub_id;
+        INNER JOIN publications.publishers ON publications.titles.pub_id = publications.publishers.pub_id
+        ORDER BY publications.authors.au_id;
 #Challenge 2
 SELECT publications.authors.au_id,publications.authors.au_lname,
 		publications.authors.au_fname,publications.publishers.pub_name, 
