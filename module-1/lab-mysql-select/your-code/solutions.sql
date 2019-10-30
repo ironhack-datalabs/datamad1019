1031740f1f43fbbf8ae5068c58bcdb60c924bf6a
jgph91 <jgph91@gmail.com>
2019-10-30 16:18:36 +0100

    v5

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -7,7 +7,7 @@ INNER JOIN titles AS t ON t.title_id = ta.title_id
 INNER JOIN publishers AS p ON p.pub_id = t.pub_id 
 
 
-#challenge 2
+# challenge 2
 
 SELECT au_fname as Nombre, au_lname as Apellido, pub_name as Editorial, count(*) as Total_titulos
 FROM authors AS a
@@ -17,6 +17,15 @@ INNER JOIN publishers AS p ON p.pub_id = t.pub_id
 GROUP BY t.pub_id, a.au_id
 
 
-
 #challenge 3
+
+SELECT a.au_fname as Nombre, a.au_lname as Apellido,sum(qty) as Total
+FROM authors AS a
+INNER JOIN titleauthor AS ta ON a.au_id = ta.au_id
+INNER JOIN titles AS t ON t.title_id = ta.title_id
+INNER JOIN sales as s ON s.title_id = t.title_id
+group by a.au_id
+ORDER BY Total DESC
+LIMIT 3
+
 
