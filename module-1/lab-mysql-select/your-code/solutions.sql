fdffae3dff6c34051502cf7689237d5dcf6d3ddc
blancaalcala <blancaalcala97@gmail.com>
2019-10-30 10:56:52 +0100

    final

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -14,7 +14,7 @@ authors.au_id AS "AUTHOR ID", authors.au_lname AS "LAST NAME", authors.au_fname 
 publishers.pub_name AS "PUBLISHER", count(*) FROM authors
 LEFT JOIN titleauthor AS ta ON authors.au_id=ta.au_id
 LEFT JOIN titles ON ta.title_id=titles.title_id
-JOIN publishers ON titles.pub_id=publishers.pub_id
+LEFT JOIN publishers ON titles.pub_id=publishers.pub_id
 GROUP BY publishers.pub_name, authors.au_id;
 
 
@@ -31,20 +31,21 @@ LIMIT 3;
 ##### CHALLENGE 4
 
 SELECT authors.au_id AS "AUTHOR ID", authors.au_lname AS "LAST NAME", authors.au_fname AS "FIRST NAME",
-SUM(sales.qty) FROM authors
+SUM(sales.qty) AS TOP3 FROM authors
 LEFT JOIN titleauthor AS ta ON authors.au_id=ta.au_id
 LEFT JOIN sales ON ta.title_id=sales.title_id
 GROUP BY authors.au_id
 ORDER BY COUNT(*) DESC
 
-##### CHALLENGE 5
+###### BONUS
 
--- SELECT authors.au_id AS "AUTHOR ID", authors.au_lname AS "LAST NAME", authors.au_fname AS "FIRST NAME" FROM authors
--- JOIN titleauthor AS ta ON authors.au_id=ta.au_id
--- -- JOIN titles ON authors.advance=titles.advance
--- JOIN titles ON authors.royalty=titles.royalty
--- GROUP BY authors.au_id
--- LIMIT 3;
+SELECT authors.au_id AS "AUTHOR ID", authors.au_lname AS "LAST NAME",
+authors.au_fname AS "FIRST NAME", SUM(titles.advance+titles.royalty) AS PROFIT FROM authors
+LEFT JOIN titleauthor AS ta ON authors.au_id=ta.au_id
+LEFT JOIN titles ON ta.title_id=titles.title_id
+GROUP BY authors.au_id
+ORDER BY COUNT(*) DESC
+LIMIT 3;
 
 
 
