4fdec70e280f30c645339914a77454afea8a9f38
Celia Prieto <cprietosegura@gmail.com>
2019-10-30 10:45:11 +0100

    challenge 1

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -1,1 +1,14 @@
-SELECT*FROM authors;
+#Challenge 1
+
+SELECT 
+		authors.au_id as 'AUTHOR ID', 
+		authors.au_lname as 'LAST NAME', 
+        authors.au_fname as 'FIRST NAME', 
+        titles.title as 'TITLE', 
+        publishers.pub_name as 'PUBLISHER'
+        
+FROM authors
+LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
+LEFT JOIN titles ON titles.title_id = titleauthor.title_id
+LEFT JOIN publishers ON titles.pub_id = publishers.pub_id
+    
