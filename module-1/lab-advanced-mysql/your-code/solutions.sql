e4631570ad06345ea32b832c9b95a2741eb7c61d
Celia Prieto <cprietosegura@gmail.com>
2019-10-30 19:54:18 +0100

    Completado el challenge 2 y terminado

--- module-1/lab-advanced-mysql/your-code/solutions.sql
+++ module-1/lab-advanced-mysql/your-code/solutions.sql
@@ -37,6 +37,33 @@ ORDER BY PROFIT DESC
 LIMIT 3;
 
 #CHALLENGE 2
+SELECT 
+		paso2.AUTHOR_ID, 
+		SUM((titles.advance*titleauthor.royaltyper/100)+ paso2.TOTAL_ROYALTIES) as 'PROFIT'
+       
+FROM (SELECT 
+		paso1.AUTHOR_ID, 
+		paso1.TITLE_ID,       
+        SUM(ROYALTY) as 'TOTAL_ROYALTIES'
+       
+FROM (SELECT 
+		authors.au_id as 'AUTHOR_ID', 
+		titles.title_id AS 'TITLE_ID',       
+        (titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) as 'ROYALTY'
+       
+FROM authors
+LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
+LEFT JOIN titles ON titles.title_id = titleauthor.title_id
+LEFT JOIN sales ON titles.title_id = sales.title_id) paso1
+
+GROUP BY paso1.AUTHOR_ID, paso1.TITLE_ID) paso2
+
+LEFT JOIN titles ON titles.title_id = paso2.TITLE_ID
+LEFT JOIN titleauthor ON titles.title_id = titleauthor.title_id
+GROUP BY paso2.AUTHOR_ID
+ORDER BY PROFIT DESC
+LIMIT 3;
+
 
 
 #CHALLENGE 3
