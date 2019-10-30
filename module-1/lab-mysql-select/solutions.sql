10ff21a2ff439c580ed16f7515412ae92db68126
albertovpd <a.vargas.pina@gmail.com>
2019-10-30 17:37:42 +0100

    challenge 2 terminado

--- module-1/lab-mysql-select/solutions.sql
+++ module-1/lab-mysql-select/solutions.sql
@@ -7,5 +7,20 @@ RIGHT JOIN titleauthor 	ON  authors.au_id=titleauthor.au_id
 LEFT JOIN titles  ON titleauthor.title_id=titles.title_id
 LEFT JOIN publishers ON titles.pub_id=publishers.pub_id;
 
-CHALLENGE 2
+# CHALLENGE 2
+SELECT authors.au_id AS "AUTHOR ID", 
+authors.au_lname AS "LAST NAME", 
+authors.au_fname AS "FIRST NAME",
+
+count(*) AS "TITLE COUNT"
+FROM authors
+inner JOIN titleauthor 	ON  authors.au_id=titleauthor.au_id 
+LEFT JOIN titles  ON titleauthor.title_id=titles.title_id
+JOIN publishers ON titles.pub_id=publishers.pub_id
+GROUP BY authors.au_id,  publishers.pub_name
+ORDER BY count(titles.title) DESC
+;
+
+
+
 
