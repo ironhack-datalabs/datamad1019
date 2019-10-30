a05819fa24e48785a511cf203136063518cdedbc
castares <cesar.castares@gmail.com>
2019-10-30 19:50:17 +0100

    challenge 2 done

--- module-1/lab-advanced-mysql/solutions.sql
+++ module-1/lab-advanced-mysql/solutions.sql
@@ -1,10 +1,13 @@
-SELECT ar.au_id, ar.title_id, sum(the_royalty*qty) AS ag_royalties
+
+# Challenge 2
+SELECT au_id, (SUM(royalties)+SUM(advance)) as profit 
 FROM (
-SELECT au.au_id, t.title_id, ti.price * s.qty * ti.royalty / 100 * t.royaltyper / 100 AS 'the_royalty', s.qty
+SELECT au.au_id, t.title_id, ti.price * s.qty * ti.royalty / 100 * t.royaltyper / 100 AS 'royalties', ti.advance*s.qty AS 'advance'
 FROM authors au
 JOIN titleauthor t ON au.au_id = t.au_id
 LEFT JOIN sales s ON t.title_id = s.title_id
 LEFT JOIN titles ti ON t.title_id = ti.title_id
 GROUP BY au.au_id, t.title_id, s.qty
 order by au.au_id ASC) as ar
-group by ar.au_id, ar.title_id, the_royalty
+group by au_id
+order by profit desc
