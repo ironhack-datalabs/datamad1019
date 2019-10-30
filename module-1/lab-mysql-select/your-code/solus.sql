0a3529d0cd0121465c505c4fad63bb2ca2a6bdae
Celia Prieto <cprietosegura@gmail.com>
2019-10-30 16:41:15 +0100

    challenge 3 completado

--- module-1/lab-mysql-select/your-code/solus.sql
+++ module-1/lab-mysql-select/your-code/solus.sql
@@ -25,3 +25,20 @@ LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
 LEFT JOIN titles ON titles.title_id = titleauthor.title_id
 INNER JOIN publishers ON titles.pub_id = publishers.pub_id
 GROUP BY publishers.pub_name, authors.au_id;
+
+
+#Challenge 3
+SELECT 
+		authors.au_id as 'AUTHOR ID', 
+		authors.au_lname as 'LAST NAME', 
+        authors.au_fname as 'FIRST NAME',
+        titles.title_id as 'TITLE id', 
+        SUM(sales.qty) as 'TOTAL'
+        
+FROM authors
+LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
+			LEFT JOIN titles ON titles.title_id = titleauthor.title_id
+						LEFT JOIN sales ON titles.title_id = sales.title_id
+GROUP BY authors.au_id
+ORDER BY TOTAL DESC
+LIMIT 3
