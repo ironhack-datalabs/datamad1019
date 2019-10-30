b55b2d46d60eb7c74b02bd648c874f22a3079925
Joaquin <jperezdeayala@gmail.com>
2019-10-30 16:24:48 +0100

    challenge 3

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -14,3 +14,13 @@ INNER JOIN publishers ON publishers.pub_id=titles.pub_id
 GROUP BY publishers.pub_id, authors.au_id;
 
 --Challenge 3
+SELECT authors.au_id AS 'AUTHOR ID', authors.au_lname AS 'LAST NAME', authors.au_fname AS 'FIRST NAME', SUM(sales.qty) AS 'TOTAL'  
+FROM authors
+INNER JOIN titleauthor ON titleauthor.au_id=authors.au_id
+INNER JOIN titles ON titles.title_id=titleauthor.title_id
+INNER JOIN sales ON sales.title_id=titles.title_id
+GROUP BY authors.au_id
+ORDER BY SUM(sales.qty) DESC
+LIMIT 3;
+
+
