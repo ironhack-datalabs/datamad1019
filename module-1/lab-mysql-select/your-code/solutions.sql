9eab0478162b8fe0fc296c0550c3ff23e5deaf4a
blancaalcala <blancaalcala97@gmail.com>
2019-10-30 10:30:19 +0100

    lab select

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -1,39 +1,41 @@
 ##### CHALLENGE 1
 
--- SELECT 
--- authors.au_id AS "AUTHOR ID", authors.au_lname AS "LAST NAME", authors.au_fname AS "FIRST NAME",
--- titles.title AS "TITLE", publishers.pub_name AS "PUBLISHER" FROM authors
--- JOIN titleauthor AS ta ON authors.au_id=ta.au_id
--- JOIN titles ON ta.title_id=titles.title_id
--- JOIN publishers ON titles.pub_id=publishers.pub_id;
+SELECT 
+authors.au_id AS "AUTHOR ID", authors.au_lname AS "LAST NAME", authors.au_fname AS "FIRST NAME",
+titles.title AS "TITLE", publishers.pub_name AS "PUBLISHER" FROM authors
+LEFT JOIN titleauthor AS ta ON authors.au_id=ta.au_id
+LEFT JOIN titles ON ta.title_id=titles.title_id
+LEFT JOIN publishers ON titles.pub_id=publishers.pub_id;
 
 ##### CHALLENGE 2
 
--- SELECT 
--- authors.au_id AS "AUTHOR ID", authors.au_lname AS "LAST NAME", authors.au_fname AS "FIRST NAME",
--- publishers.pub_name AS "PUBLISHER", count(*) FROM authors
--- JOIN titleauthor AS ta ON authors.au_id=ta.au_id
--- JOIN titles ON ta.title_id=titles.title_id
--- JOIN publishers ON titles.pub_id=publishers.pub_id
--- GROUP BY publishers.pub_name, authors.au_id;
+SELECT 
+authors.au_id AS "AUTHOR ID", authors.au_lname AS "LAST NAME", authors.au_fname AS "FIRST NAME",
+publishers.pub_name AS "PUBLISHER", count(*) FROM authors
+LEFT JOIN titleauthor AS ta ON authors.au_id=ta.au_id
+LEFT JOIN titles ON ta.title_id=titles.title_id
+JOIN publishers ON titles.pub_id=publishers.pub_id
+GROUP BY publishers.pub_name, authors.au_id;
 
 
 ##### CHALLENGE 3
 
--- SELECT authors.au_id AS "AUTHOR ID", authors.au_lname AS "LAST NAME", authors.au_fname AS "FIRST NAME",
--- IFNULL(COUNT(*), 0) FROM authors
--- JOIN titleauthor AS ta ON authors.au_id=ta.au_id
--- GROUP BY authors.au_id
--- ORDER BY COUNT(*) DESC
--- LIMIT 3;
+SELECT authors.au_id AS "AUTHOR ID", authors.au_lname AS "LAST NAME", authors.au_fname AS "FIRST NAME",
+SUM(sales.qty) FROM authors
+LEFT JOIN titleauthor AS ta ON authors.au_id=ta.au_id
+LEFT JOIN sales ON ta.title_id=sales.title_id
+GROUP BY authors.au_id
+ORDER BY COUNT(*) DESC
+LIMIT 3;
 
 ##### CHALLENGE 4
 
--- SELECT authors.au_id AS "AUTHOR ID", authors.au_lname AS "LAST NAME", authors.au_fname AS "FIRST NAME",
--- IFNULL(COUNT(*), 0) FROM authors
--- JOIN titleauthor AS ta ON authors.au_id=ta.au_id
--- GROUP BY authors.au_id
--- ORDER BY IFNULL(COUNT(*), 0) DESC;
+SELECT authors.au_id AS "AUTHOR ID", authors.au_lname AS "LAST NAME", authors.au_fname AS "FIRST NAME",
+SUM(sales.qty) FROM authors
+LEFT JOIN titleauthor AS ta ON authors.au_id=ta.au_id
+LEFT JOIN sales ON ta.title_id=sales.title_id
+GROUP BY authors.au_id
+ORDER BY COUNT(*) DESC
 
 ##### CHALLENGE 5
 
