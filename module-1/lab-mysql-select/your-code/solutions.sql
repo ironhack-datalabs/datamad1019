eabe971853212e23bcdc5ade1c193bcffc27003d
Celia Prieto <cprietosegura@gmail.com>
2019-10-30 16:57:44 +0100

    cambiando nombre de fichero

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -1,4 +1,17 @@
 
+#Challenge 1
+SELECT 
+		authors.au_id as 'AUTHOR ID', 
+		authors.au_lname as 'LAST NAME', 
+        authors.au_fname as 'FIRST NAME', 
+        titles.title as 'TITLE', 
+        publishers.pub_name as 'PUBLISHER'
+        
+FROM authors
+LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
+LEFT JOIN titles ON titles.title_id = titleauthor.title_id
+INNER JOIN publishers ON titles.pub_id = publishers.pub_id;
+
 #Challenge 2
 SELECT 
 		authors.au_id as 'AUTHOR ID', 
@@ -9,9 +22,37 @@ SELECT
         
 FROM authors
 LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
-			LEFT JOIN titles ON titles.title_id = titleauthor.title_id
-						INNER JOIN publishers ON titles.pub_id = publishers.pub_id
+LEFT JOIN titles ON titles.title_id = titleauthor.title_id
+INNER JOIN publishers ON titles.pub_id = publishers.pub_id
 GROUP BY publishers.pub_name, authors.au_id;
 
 
-    
+#Challenge 3
+SELECT 
+		authors.au_id as 'AUTHOR ID', 
+		authors.au_lname as 'LAST NAME', 
+        authors.au_fname as 'FIRST NAME',
+        SUM(sales.qty) as 'TOTAL'
+        
+FROM authors
+LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
+			LEFT JOIN titles ON titles.title_id = titleauthor.title_id
+						LEFT JOIN sales ON titles.title_id = sales.title_id
+GROUP BY authors.au_id
+ORDER BY TOTAL DESC
+LIMIT 3
+
+#Challenge 4
+SELECT 
+		authors.au_id as 'AUTHOR ID', 
+		authors.au_lname as 'LAST NAME', 
+        authors.au_fname as 'FIRST NAME',
+        IFNULL(SUM(sales.qty),0) as 'TOTAL'
+        
+FROM authors
+LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
+			LEFT JOIN titles ON titles.title_id = titleauthor.title_id
+						LEFT JOIN sales ON titles.title_id = sales.title_id
+GROUP BY authors.au_id
+ORDER BY TOTAL DESC
+LIMIT 23
