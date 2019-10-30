2d7eddaabf92cf50e29e2e68039dcf1c7e7266fc
mariaadradosr <mariaadradosr@gmail.com>
2019-10-30 12:17:42 +0100

    commit resolución completa versión 3

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -21,32 +21,25 @@ left join publishers p
 
 # Challenge 2: Who Have Published How Many At Where?
 
-select	Author_id,
-		Last_name,
-		First_name, 
-		Publisher, 
-		count(*) as 'Title_count'
-from (
-		select 	f.au_id as Author_id, 
-				f.au_lname as Last_name, 
-				f.au_fname as First_name, 
-				t.title as Title, 
-				p.pub_name as Publisher
-		from titles t 
-		left join (
-					select ta.au_id, 
-							a.au_lname, 
-							a.au_fname,
-							ta.title_id
-					from titleauthor ta
-					left join authors a
-						on ta.au_id = a.au_id
-						) f
-			on t.title_id = f.title_id
-		left join publishers p
-			on t.pub_id = p.pub_id
-		) foo
-group by 1, 2, 3;
+select 	f.au_id as Author_id, 
+		f.au_lname as Last_name, 
+		f.au_fname as First_name, 
+		p.pub_name as Publisher,
+        count(t.title) as Title
+from titles t 
+left join (
+			select ta.au_id, 
+					a.au_lname, 
+					a.au_fname,
+					ta.title_id
+			from titleauthor ta
+			left join authors a
+				on ta.au_id = a.au_id
+				) f
+	on t.title_id = f.title_id
+left join publishers p
+	on t.pub_id = p.pub_id
+group by 1, 2, 3, 4;
 
 # Challenge 3: Best Selling Authors
 select	a.au_id as Author_id, 
@@ -103,3 +96,4 @@ left join
 left join titles t
 	on ta.title_id = t.title_id
 group by 1, 2, 3;
+
