6ac2a1c18203c412fa07f13744006dfcc0c32c33
jgph91 <jgph91@gmail.com>
2019-10-30 10:12:57 +0100

    v4

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -9,18 +9,14 @@ INNER JOIN publishers AS p ON p.pub_id = t.pub_id
 
 #challenge 2
 
-SELECT au_lname, count(title)
+SELECT au_fname as Nombre, au_lname as Apellido, pub_name as Editorial, count(*) as Total_titulos
 FROM authors AS a
 INNER JOIN titleauthor AS ta ON a.au_id = ta.au_id
 INNER JOIN titles AS t ON t.title_id = ta.title_id
 INNER JOIN publishers AS p ON p.pub_id = t.pub_id 
-GROUP BY au_lname
+GROUP BY t.pub_id, a.au_id
+
+
 
 #challenge 3
 
-SELECT au_fname,qty
-FROM authors AS a
-INNER JOIN titleauthor AS ta ON a.au_id = ta.au_id
-INNER JOIN titles AS t ON t.title_id = ta.title_id
-INNER JOIN sales as s ON s.title_id = ta.title_id
-ORDER BY qty desc
