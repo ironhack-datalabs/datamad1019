12136ae669bfa8c33b095b74ffcea527813d7f27
mariobru <mariobrubarrero@gmail.com>
2019-10-30 16:53:31 +0100

    Challenge 4 done.

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -37,6 +37,14 @@ limit 3;
 
 # Challenge 4
 
+select a.au_id as AUTHOR_ID, a.au_lname as LAST_NAME, a.au_fname as FIRST_NAME, ifnull(sum(s.qty),0) as TOTAL
+from publications.authors a
+left join publications.titleauthor ta
+on a.au_id = ta.au_id
+left join publications.sales s
+on ta.title_id = s.title_id
+group by a.au_id
+order by TOTAL desc;
 
 
 
