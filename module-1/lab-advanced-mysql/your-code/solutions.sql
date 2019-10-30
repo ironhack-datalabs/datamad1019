48cca72a118349b3019ac50048e89477efde865b
Ovi <jm.fdez.corbacho@gmail.com>
2019-10-31 00:18:51 +0100

    Añadido challenge 2 corregido

--- module-1/lab-advanced-mysql/your-code/solutions.sql
+++ module-1/lab-advanced-mysql/your-code/solutions.sql
@@ -60,48 +60,19 @@
 -- 	SELECT 
 -- 		titleauthor.title_id AS "Title_ID",
 -- 		titleauthor.au_id AS "Author_ID",
--- 		(titles.price * sales.qty * titles.royalty/100 * titleauthor.royaltyper/100) AS "Sales_Royalty",
+-- 		SUM((titles.price * sales.qty * titles.royalty/100 * titleauthor.royaltyper/100)) AS "Sales_Royalty",
 -- 		titles.advance AS "Advance",
 -- 		titleauthor.royaltyper AS "Royal_Typer"
 -- 		FROM titleauthor
 -- 		LEFT JOIN sales ON titleauthor.title_id = sales.title_id
--- 		LEFT JOIN titles ON sales.title_id = titles.title_id;
-
--- SELECT
+-- 		LEFT JOIN titles ON sales.title_id = titles.title_id
+--         GROUP BY Title_ID, Author_ID;
+-- 
+-- SELECT 
 -- 	Author_ID,
---     SUM(Sales_Royalty + Advance*Royal_Typer/100) AS "Profits"
--- 	FROM (
--- 		SELECT
--- 			Base.Title_ID AS "Title_ID",
--- 			Base.Author_ID AS "Author_ID",
--- 			SUM(Base.Sales_Royalty) AS "Sales_Royalty",
---             Base.Advance AS "Advance",
---             Base.Royal_Typer AS "Royal_Typer"
--- 			FROM Base
--- 			GROUP BY Base.Title_ID, Base.Author_ID
--- 	) AS y
--- 	GROUP BY Author_ID
---     ORDER BY Profits DESC
---     LIMIT 3
-
-
-#oor
--- SELECT
--- 	Author_ID,
---     SUM(Sales_Royalty + Advance*Royal_Typer/100) AS "Profits"
--- 	FROM (
--- 		SELECT
--- 			Title_ID AS "Title_ID",
--- 			Author_ID AS "Author_ID",
--- 			SUM(Sales_Royalty) AS "Sales_Royalty",
---             Advance AS "Advance",
---             Royal_Typer AS "Royal_Typer"
--- 			FROM Base
--- 			GROUP BY Title_ID, Author_ID
--- 	) AS y
--- 	GROUP BY Author_ID
---     ORDER BY Profits DESC
---     LIMIT 3
+--     SUM(Sales_Royalty + Advance * Royal_Typer/100) AS "Profit"
+-- 	FROM Base
+--     GROUP BY Author_ID;
 
 '''
 #challenge 3
