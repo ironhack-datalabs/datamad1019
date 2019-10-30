9f21a59dc4e0e3ddeb6d47c292e1e265e91cf994
Antonio Pons <toninopons@gmail.com>
2019-10-30 19:03:11 +0100

    Bonus listo

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -26,3 +26,15 @@ INNER JOIN publishers ON titles.pub_id = publishers.pub_id
 LEFT JOIN sales ON titles.title_id = sales.title_id
 GROUP BY authors.au_id
 ORDER BY SUM(sales.qty) DESC;
+
+#BONUS
+
+SELECT authors.au_id, au_lname, au_fname, SUM(sales.qty * roysched.royalty / 100 * titleauthor.royaltyper / 100) + SUM(titles.advance * titleauthor.royaltyper / 100) as profits
+FROM authors 
+INNER JOIN titleauthor ON authors.au_id = titleauthor.au_id
+INNER JOIN titles ON titleauthor.title_id = titles.title_id
+INNER JOIN publishers ON titles.pub_id = publishers.pub_id
+LEFT JOIN sales ON titles.title_id = sales.title_id
+LEFT JOIN roysched ON sales.title_id = roysched.title_id
+GROUP BY authors.au_id
+ORDER BY profits DESC;
