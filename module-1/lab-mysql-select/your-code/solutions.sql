50a661622cc35e4d8d0bb306963d01a305c748ae
jgph91 <jgph91@gmail.com>
2019-10-30 16:23:07 +0100

    v6

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -28,4 +28,15 @@ group by a.au_id
 ORDER BY Total DESC
 LIMIT 3
 
+#challenge 4
+
+SELECT a.au_fname as Nombre, a.au_lname as Apellido,sum(qty) as Total
+FROM authors AS a
+INNER JOIN titleauthor AS ta ON a.au_id = ta.au_id
+INNER JOIN titles AS t ON t.title_id = ta.title_id
+INNER JOIN sales as s ON s.title_id = t.title_id
+group by a.au_id
+ORDER BY Total DESC
+LIMIT 23
+
 
