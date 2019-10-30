fa4455aeed24c41e781761a3b05a3c2fabb43740
alfpml <alfpml@gmail.com>
2019-10-30 16:55:39 +0100

    final commit with Bonus

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -76,3 +76,70 @@ au.au_id
 ,au.au_lname
 ,au.au_fname) T1
 ORDER BY TITLE_Count DESC
+
+--Challenge 4 - Best Selling Authors Ranking
+
+SELECT*
+FROM(
+SELECT 
+au.au_id
+,au.au_lname
+,au.au_fname
+,sum(t4.au_ti_profit) as profit_total
+FROM (
+SELECT 
+ta.au_id
+,t3.title_id
+,t3.title
+,t3.profit AS Ti_Profit
+,ta.royaltyper
+,Round((ifnull(ta.royaltyper,0)/100)*t3.profit,2) AS au_ti_profit
+FROM (
+	SELECT 
+	t2.title_id
+	,t2.title
+	,(t2.price*t2.qty*(t2.royalty/100) + t2.advance) AS Profit
+FROM (
+		SELECT
+		t1.title_id
+		,t1.title
+		,t1.price
+		,t1.advance
+		,rs.royalty
+		,t1.royalty as royalty2
+		,t1.ytd_sales
+		,t1.qty
+FROM 
+			(
+			SELECT
+			ti.title_id
+			,ti.title
+			,ifnull(ti.price,0) AS price
+			,ifnull(ti.advance,0) AS advance
+			,ti.royalty
+			,ti.ytd_sales
+			,SUM(ifnull(ss.qty,0)) AS qty
+			FROM titles ti
+			LEFT JOIN sales ss ON ti.title_id=ss.title_id
+			GROUP BY
+			ti.title_id
+			,ti.title
+			,ifnull(ti.price,0)
+			,ifnull(ti.advance,0)
+			,ti.royalty
+			,ti.ytd_sales
+			) t1
+		LEFT JOIN roysched rs ON t1.title_id=rs.title_id AND t1.ytd_sales>rs.lorange and t1.ytd_sales<rs.hirange
+	) t2
+) t3
+LEFT JOIN titleauthor ta on t3.title_id=ta.title_id
+) t4
+
+INNER JOIN authors au on au.au_id=t4.au_id
+GROUP BY
+au.au_id
+,au.au_lname
+,au.au_fname
+) T5
+ORDER BY T5.profit_Total DESC LIMIT 3
+
