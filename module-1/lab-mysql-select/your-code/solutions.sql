bac8bf977eb221cf99bf776ffb70b38295a48fa9
Joaquin <jperezdeayala@gmail.com>
2019-10-30 15:35:03 +0100

    challenge 1 corregido

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -1,6 +1,8 @@
 --Challenge 1
 SELECT authors.au_id AS 'AUTHOR ID', authors.au_lname AS 'LAST NAME', authors.au_fname AS 'FIRST NAME', titles.title AS 'TITLE', publishers.pub_name AS 'PUBLISHER'  
 FROM authors
-LEFT JOIN titleauthor ON titleauthor.au_id=authors.au_id
+INNER JOIN titleauthor ON titleauthor.au_id=authors.au_id
 LEFT JOIN titles ON titles.title_id=titleauthor.title_id
 LEFT JOIN publishers ON publishers.pub_id=titles.pub_id;
+
+-- Challenge 2
