39226b04f9f80d35353a57bb2ba234ba32d4f932
Cristina Rota <cristina.rota.m@gmail.com>
2019-10-30 10:31:22 +0100

    Updating my solutions

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -3,23 +3,44 @@
 
 # Challenge 1 - Who Have Published What At Where?
 
+# Since the total rows in my output should be the same as the total number of records in
+# Table titleauthor, I'm doing a LEFT JOIN from titleauthor.
+
 SELECT
-	au.au_id AS "AUTHOR ID", au.au_lname AS "LAST NAME", au.au_fname AS "FIRST NAME", t.title AS "TITLE", p.pub_name AS "PUBLISHER"
+	au.au_id AS "AUTHOR ID",
+    au.au_lname AS "LAST NAME",
+    au.au_fname AS "FIRST NAME",
+    t.title AS "TITLE",
+    p.pub_name AS "PUBLISHER"
     FROM titleauthor AS ta
     LEFT JOIN titles AS t ON ta.title_id = t.title_id
     LEFT JOIN authors AS au ON ta.au_id = au.au_id
     LEFT JOIN publishers AS p ON t.pub_id = p.pub_id;
-    
-# If your query is correct, the total rows in your output should be the same as the total
-# number of records in Table titleauthor.
-# SELECT * FROM titleauthor; # 25 rows returned == My output
+# SELECT * FROM titleauthor; # (25 row(s) returned) == My output (25 row(s) returned)
+
+# I could also do a RIGHT JOIN from authors:
+SELECT
+	au.au_id AS "AUTHOR ID",
+    au.au_lname AS "LAST NAME",
+    au.au_fname AS "FIRST NAME",
+    t.title AS "TITLE",
+    p.pub_name AS "PUBLISHER"
+    FROM authors AS au
+    RIGHT JOIN titleauthor as ta ON au.au_id = ta.au_id
+    LEFT JOIN titles AS t ON ta.title_id = t.title_id
+    LEFT JOIN publishers AS p ON t.pub_id = p.pub_id;
+# SELECT * FROM titleauthor; # (25 row(s) returned) == My output (25 row(s) returned)
 
 ########################################################################################
 
 # Challenge 2 - Who Have Published How Many At Where?
   
 SELECT
-	au.au_id AS "AUTHOR ID", au.au_lname AS "LAST NAME", au.au_fname AS "FIRST NAME", p.pub_name AS "PUBLISHER", count(*) AS "TITLE COUNT"
+	au.au_id AS "AUTHOR ID",
+    au.au_lname AS "LAST NAME",
+    au.au_fname AS "FIRST NAME",
+    p.pub_name AS "PUBLISHER",
+    count(*) AS "TITLE COUNT"
     FROM titleauthor AS ta
     LEFT JOIN titles AS t ON ta.title_id = t.title_id
     LEFT JOIN authors AS au ON ta.au_id = au.au_id
@@ -39,7 +60,10 @@ SELECT
 # Who are the top 3 authors who have sold the highest number of titles? Write a query to find out.
     
 SELECT
-	au.au_id AS "AUTHOR ID", au.au_lname AS "LAST NAME", au.au_fname AS "FIRST NAME", SUM(s.qty) AS "TOTAL"
+	au.au_id AS "AUTHOR ID",
+    au.au_lname AS "LAST NAME",
+    au.au_fname AS "FIRST NAME",
+    SUM(s.qty) AS "TOTAL"
     FROM authors AS au
     LEFT JOIN titleauthor AS ta ON au.au_id = ta.au_id
     LEFT JOIN titles AS t ON ta.title_id = t.title_id
@@ -47,24 +71,30 @@ SELECT
 	GROUP BY au.au_id
     ORDER BY SUM(s.qty) DESC
     LIMIT 3;
-
-# Con esta otra manera no selecciono las 4 filas con valores nulos (devuelve solo 19 filas):
-# SELECT
-	# au.au_id AS "AUTHOR ID", au.au_lname AS "LAST NAME", au.au_fname AS "FIRST NAME", SUM(s.qty) AS "TOTAL"
-    # FROM titleauthor AS ta
-    # LEFT JOIN titles AS t ON ta.title_id = t.title_id
-    # LEFT JOIN sales AS s ON ta.title_id = s.title_id
-    # LEFT JOIN authors AS au ON ta.au_id = au.au_id
-	# GROUP BY au.au_id
-    # ORDER BY SUM(s.qty) DESC
-    # LIMIT 3;
+    
+# I could also do a RIGHT JOIN from titleauthor:
+SELECT
+	au.au_id AS "AUTHOR ID",
+    au.au_lname AS "LAST NAME",
+    au.au_fname AS "FIRST NAME",
+    SUM(s.qty) AS "TOTAL"
+    FROM titleauthor AS ta
+    RIGHT JOIN authors AS au ON ta.au_id = au.au_id
+    LEFT JOIN titles AS t ON ta.title_id = t.title_id
+    LEFT JOIN sales AS s ON ta.title_id = s.title_id
+	GROUP BY au.au_id
+    ORDER BY SUM(s.qty) DESC
+    LIMIT 3;
 
 ########################################################################################
 
 # Challenge 4 - Best Selling Authors Ranking
 
 SELECT
-	au.au_id AS "AUTHOR ID", au.au_lname AS "LAST NAME", au.au_fname AS "FIRST NAME", IFNULL(SUM(s.qty), 0) AS "TOTAL" 
+	au.au_id AS "AUTHOR ID",
+    au.au_lname AS "LAST NAME",
+    au.au_fname AS "FIRST NAME",
+    IFNULL(SUM(s.qty), 0) AS "TOTAL" 
     FROM authors AS au
     LEFT JOIN titleauthor AS ta ON au.au_id = ta.au_id
     LEFT JOIN titles AS t ON ta.title_id = t.title_id
