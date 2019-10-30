3f4fd6cc34382207dadb87b5af484c954e64e3fe
Celia Prieto <cprietosegura@gmail.com>
2019-10-30 16:54:25 +0100

    challenge 4 completado

--- module-1/lab-mysql-select/your-code/solus.sql
+++ module-1/lab-mysql-select/your-code/solus.sql
@@ -32,7 +32,6 @@ SELECT
 		authors.au_id as 'AUTHOR ID', 
 		authors.au_lname as 'LAST NAME', 
         authors.au_fname as 'FIRST NAME',
-        titles.title_id as 'TITLE id', 
         SUM(sales.qty) as 'TOTAL'
         
 FROM authors
@@ -42,3 +41,18 @@ LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
 GROUP BY authors.au_id
 ORDER BY TOTAL DESC
 LIMIT 3
+
+#Challenge 4
+SELECT 
+		authors.au_id as 'AUTHOR ID', 
+		authors.au_lname as 'LAST NAME', 
+        authors.au_fname as 'FIRST NAME',
+        IFNULL(SUM(sales.qty),0) as 'TOTAL'
+        
+FROM authors
+LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
+			LEFT JOIN titles ON titles.title_id = titleauthor.title_id
+						LEFT JOIN sales ON titles.title_id = sales.title_id
+GROUP BY authors.au_id
+ORDER BY TOTAL DESC
+LIMIT 23
