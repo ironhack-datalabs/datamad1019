6e1f7fd6d3afa2902d22c19e85970cef31ef21cd
Joaquin <jperezdeayala@gmail.com>
2019-10-30 16:38:19 +0100

    challenge 4

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -23,4 +23,13 @@ GROUP BY authors.au_id
 ORDER BY SUM(sales.qty) DESC
 LIMIT 3;
 
+-- Challenge 4
+SELECT authors.au_id AS 'AUTHOR ID', authors.au_lname AS 'LAST NAME', authors.au_fname AS 'FIRST NAME', IFNULL(SUM(sales.qty),0) AS 'TOTAL'  
+FROM authors
+LEFT JOIN titleauthor ON titleauthor.au_id=authors.au_id
+LEFT JOIN titles ON titles.title_id=titleauthor.title_id
+LEFT JOIN sales ON sales.title_id=titles.title_id
+GROUP BY authors.au_id
+ORDER BY SUM(sales.qty) DESC;
+
 
