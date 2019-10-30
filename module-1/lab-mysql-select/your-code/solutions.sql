2f7f927342e9f79d91bf4631795ff5e19c5dd348
mariobru <mariobrubarrero@gmail.com>
2019-10-30 16:09:14 +0100

    Challenge 2 bien

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -12,25 +12,20 @@ order by AUTHOR_ID;
 
 # Challenge 2
 
-#Si da error ejecutad esto:
--- set
--- sql_mode=
--- 'STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
-
 select a.au_id as AUTHOR_ID, a.au_lname as LAST_NAME, a.au_fname as FIRST_NAME, p.pub_name as PUBLISHER, count(*) as TITLE_COUNT
 from publications.authors a
 left join publications.titleauthor ta
 on a.au_id = ta.au_id
-left join publications.titles t
+inner join publications.titles t
 on ta.title_id = t.title_id
 inner join publications.publishers p
 on t.pub_id = p.pub_id
-group by t.title_id
+group by p.pub_name, a.au_id
 order by AUTHOR_ID desc;
 
 # Challenge 3
 
-select a.au_id as AUTHOR_ID, a.au_lname as LAST_NAME, a.au_fname as FIRST_NAME, count(*) as TITLE
+select a.au_id as AUTHOR_ID, a.au_lname as LAST_NAME, a.au_fname as FIRST_NAME, count(*) as TOTAL
 from publications.authors a
 left join publications.titleauthor ta
 on a.au_id = ta.au_id
