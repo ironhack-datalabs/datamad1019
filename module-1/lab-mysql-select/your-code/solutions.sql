2a7e7213bbf305446778c231ab4b469b65a1d78c
Antonio Pons <toninopons@gmail.com>
2019-10-30 17:54:07 +0100

    Challenge 2 listo

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -1,5 +1,17 @@
+#Challenge 1
+
 SELECT authors.au_id, au_lname, au_fname, titles.title, publishers.pub_name
 FROM authors 
 INNER JOIN titleauthor ON authors.au_id = titleauthor.au_id
 INNER JOIN titles ON titleauthor.title_id = titles.title_id
 INNER JOIN publishers ON titles.pub_id = publishers.pub_id;
+
+#Challenge 2
+
+SELECT authors.au_id, au_lname, au_fname, publishers.pub_name, COUNT(titles.title) as total
+FROM authors 
+INNER JOIN titleauthor ON authors.au_id = titleauthor.au_id
+INNER JOIN titles ON titleauthor.title_id = titles.title_id
+INNER JOIN publishers ON titles.pub_id = publishers.pub_id
+GROUP BY authors.au_id, publishers.pub_name
+ORDER BY COUNT(titles.title) DESC;
