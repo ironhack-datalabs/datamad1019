215001f618cbb957df8c9ec8d17d8bbdf3cc2eef
albertovpd <a.vargas.pina@gmail.com>
2019-10-30 18:33:22 +0100

    ej3 terminado

--- module-1/lab-mysql-select/solutions.sql
+++ module-1/lab-mysql-select/solutions.sql
@@ -21,6 +21,20 @@ GROUP BY authors.au_id,  publishers.pub_name
 ORDER BY count(titles.title) DESC
 ;
 
+# CHALLENGE 3
+SELECT authors.au_id AS "AUTHOR ID", 
+authors.au_lname AS "LAST NAME", 
+authors.au_fname AS "FIRST NAME",
+
+SUM(sales.qty) AS "TOTAL"
+FROM authors
+inner JOIN titleauthor 	ON  authors.au_id=titleauthor.au_id 
+LEFT JOIN titles  ON titleauthor.title_id=titles.title_id
+JOIN publishers ON titles.pub_id=publishers.pub_id
+LEFT JOIN sales ON titleauthor.title_id=sales.title_id
+GROUP BY authors.au_id, authors.au_lname, authors.au_fname
+ORDER BY SUM(sales.qty) DESC
+LIMIT 3 ; 
 
 
 
