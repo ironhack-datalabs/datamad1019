bfcd98a2a0728df6f5ea2749599e63be4d8ca5f0
Celia Prieto <cprietosegura@gmail.com>
2019-10-30 16:08:04 +0100

    completado el challenge 2

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -1,14 +1,17 @@
 
-#Challenge 1
+#Challenge 2
 SELECT 
 		authors.au_id as 'AUTHOR ID', 
 		authors.au_lname as 'LAST NAME', 
         authors.au_fname as 'FIRST NAME', 
-        titles.title as 'TITLE', 
-        publishers.pub_name as 'PUBLISHER'
+        publishers.pub_name as 'PUBLISHER',
+        COUNT(titles.title) as 'TITLE COUNT'
         
 FROM authors
 LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
-LEFT JOIN titles ON titles.title_id = titleauthor.title_id
-INNER JOIN publishers ON titles.pub_id = publishers.pub_id
+			LEFT JOIN titles ON titles.title_id = titleauthor.title_id
+						INNER JOIN publishers ON titles.pub_id = publishers.pub_id
+GROUP BY publishers.pub_name, authors.au_id;
+
+
     
