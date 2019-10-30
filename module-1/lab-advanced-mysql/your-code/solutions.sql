a5fce211fec037d59317964f11ba8300c4ac798d
castares <cesar.castares@gmail.com>
2019-10-30 20:52:27 +0100

    challenge 3 pending

--- module-1/lab-advanced-mysql/your-code/solutions.sql
+++ module-1/lab-advanced-mysql/your-code/solutions.sql
@@ -1,5 +1,4 @@
-
-# Challenge 2
+# Challenge 1 & 2 ???
 SELECT au_id, (SUM(royalties)+SUM(advance)) as profit 
 FROM (
 SELECT au.au_id, t.title_id, ti.price * s.qty * ti.royalty / 100 * t.royaltyper / 100 AS 'royalties', ti.advance*s.qty AS 'advance'
@@ -12,3 +11,4 @@ order by au.au_id ASC) as ar
 group by au_id
 order by profit desc
 
+# Challenge 3
