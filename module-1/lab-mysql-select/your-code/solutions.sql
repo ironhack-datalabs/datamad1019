92ecf295954c97f5d2f6e5e6883a360f10248963
Antonio Pons <toninopons@gmail.com>
2019-10-30 18:49:10 +0100

    Challenge 3 listo, lab listo, falta el bonus.

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -15,3 +15,14 @@ INNER JOIN titles ON titleauthor.title_id = titles.title_id
 INNER JOIN publishers ON titles.pub_id = publishers.pub_id
 GROUP BY authors.au_id, publishers.pub_name
 ORDER BY COUNT(titles.title) DESC;
+
+#Challenge 3
+
+SELECT authors.au_id, au_lname, au_fname, SUM(sales.qty)
+FROM authors 
+INNER JOIN titleauthor ON authors.au_id = titleauthor.au_id
+INNER JOIN titles ON titleauthor.title_id = titles.title_id
+INNER JOIN publishers ON titles.pub_id = publishers.pub_id
+LEFT JOIN sales ON titles.title_id = sales.title_id
+GROUP BY authors.au_id
+ORDER BY SUM(sales.qty) DESC;
