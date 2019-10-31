ff96a28133635b2708eeef467705059cc817e2df
Antonio Pons <toninopons@gmail.com>
2019-10-31 16:22:53 +0100

    Primer challenge listo

--- module-1/lab-advanced-mysql/your-code/solutions.sql
+++ module-1/lab-advanced-mysql/your-code/solutions.sql
@@ -1,8 +1,9 @@
-SELECT authors.au_id, au_lname, au_fname, IFNULL(SUM(qty * royalty / 100 * royaltyper / 100 * price), 0) AS profit
+#Challenge 1
+
 FROM authors 
 LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
 LEFT JOIN titles ON titleauthor.title_id = titles.title_id
 LEFT JOIN publishers ON titles.pub_id = publishers.pub_id
 LEFT JOIN sales ON titles.title_id = sales.title_id
-GROUP BY authors.au_id
-ORDER BY SUM(sales.qty) DESC;
+GROUP BY authors.au_id, titles.title_id
+ORDER BY profit DESC;
