fc178853ae116c360b67f0bc8c1747e989fa46e7
albertovpd <a.vargas.pina@gmail.com>
2019-10-31 09:16:29 +0100

    temporary tables

--- module-1/lab-advanced-mysql/your-code/solutions.sql
+++ module-1/lab-advanced-mysql/your-code/solutions.sql
@@ -1,15 +1,37 @@
-# step1
-SELECT 
-titles.title_id AS "Title ID", 
-authors.au_id AS "Author ID",
-titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 AS "Royalty"
+# create a temporary table
 
-FROM authors
-LEFT JOIN titleauthor 	ON  authors.au_id=titleauthor.au_id 
-LEFT JOIN titles  ON titleauthor.title_id=titles.title_id
-LEFT JOIN sales ON titleauthor.title_id=sales.title_id
+create temporary table sales_per_title
+ select title_id, sum(qty) as qty
+        from sales
+        group by 1;
+select  ta.au_id as Author_id, 
+        sum(((t.royalty/100 *(t.price*s.qty)) + t.advance)*ta.royaltyper/100) as profit
+from titleauthor ta
+left join authors a
+    on ta.au_id = a.au_id
+left join sales_per_title s
+    on s.title_id = ta.title_id
+left join titles t
+    on ta.title_id = t.title_id
+group by 1
+order by profit  desc;
 
-GROUP BY titles.title_id, authors.au_id, titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100;
 
-# step2
-
+-- Método 2: Tienes que investigar más
+-- CREATE TEMPORARY TABLE Base .
+--  SELECT 
+--      titleauthor.title_id AS "Title_ID",
+--      titleauthor.au_id AS "Author_ID",
+--      SUM((titles.price * sales.qty * titles.royalty/100 * titleauthor.royaltyper/100)) AS "Sales_Royalty",
+--      titles.advance AS "Advance",
+--      titleauthor.royaltyper AS "Royal_Typer"
+--      FROM titleauthor
+--      LEFT JOIN sales ON titleauthor.title_id = sales.title_id
+--      LEFT JOIN titles ON sales.title_id = titles.title_id
+--         GROUP BY Title_ID, Author_ID;
+-- 
+-- SELECT 
+--  Author_ID,
+--     SUM(Sales_Royalty + Advance * Royal_Typer/100) AS "Profit"
+--  FROM Base
+--     GROUP BY Author_ID;
