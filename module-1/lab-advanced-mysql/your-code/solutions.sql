f964a4f1c376f62eb3c614610ec575972f8ac7ed
jgph91 <jgph91@gmail.com>
2019-10-31 07:01:48 +0100

    challenge 1 completo

--- module-1/lab-advanced-mysql/your-code/solutions.sql
+++ module-1/lab-advanced-mysql/your-code/solutions.sql
@@ -1,7 +1,7 @@
 # Challenge 1
 #Step 1
 
-SELECT titles.title_id, authors.au_id, (titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) AS sales_royalty
+SELECT titles.title_id AS ID_Titulos, authors.au_id as ID_Autores, (titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) AS sales_royalty
 FROM authors
 INNER JOIN titleauthor ON authors.au_id = titleauthor.au_id
 INNER JOIN titles ON  titles.title_id = titleauthor.title_id
@@ -9,3 +9,28 @@ INNER JOIN sales ON sales.title_id = titles.title_id
 
 #Step 2
 
+SELECT ID_Titulos,  ID_Autores, sum(sales_royalty)
+FROM (
+SELECT titles.title_id AS ID_Titulos, authors.au_id as ID_Autores, (titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) AS sales_royalty
+FROM authors
+INNER JOIN titleauthor ON authors.au_id = titleauthor.au_id
+INNER JOIN titles ON  titles.title_id = titleauthor.title_id
+INNER JOIN sales ON sales.title_id = titles.title_id) SUMMARY
+GROUP BY ID_Titulos, ID_Autores
+
+#Step 3
+
+SELECT ID_AUTORES,  Sales_total
+FROM
+(SELECT ID_Titulos,  ID_Autores, sum(sales_royalty) AS Sales_total
+FROM (
+SELECT titles.title_id AS ID_Titulos, authors.au_id as ID_Autores, (titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) AS sales_royalty
+FROM authors
+INNER JOIN titleauthor ON authors.au_id = titleauthor.au_id
+INNER JOIN titles ON  titles.title_id = titleauthor.title_id
+INNER JOIN sales ON sales.title_id = titles.title_id) SUMMARY
+GROUP BY ID_Titulos, ID_Autores) X
+ORDER BY Sales_total DESC
+LIMIT 3
+
+#Challenge 2
