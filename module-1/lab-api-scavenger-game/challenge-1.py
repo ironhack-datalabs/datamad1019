c045dfb49d2a7084c26a457fe1fe2494421d9379
Antonio Pons <toninopons@gmail.com>
2019-11-06 19:04:34 +0100

    first commit

--- module-1/lab-api-scavenger-game/challenge-1.py
+++ module-1/lab-api-scavenger-game/challenge-1.py
@@ -1,1 +1,17 @@
-# enter your code below
+import os
+from dotenv import load_dotenv
+import requests
+load_dotenv()
+def githubRequestAuthorized(resource):
+    authToken = os.getenv("AUTOKEN")
+    if not authToken:
+        raise ValueError("NECESITAS UN TOKEN")
+    else:
+        print("We have a github token: ", authToken[0:4])
+    headers = {
+        "Authorization": "token {}".format(authToken)
+    }
+    url = "https://api.github.com{}".format(resource)
+    print("Requesting authorized {}".format(url))
+    res = requests.get(url, headers=headers)
+    return res
