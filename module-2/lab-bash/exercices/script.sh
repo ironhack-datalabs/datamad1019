14f86b1fbb7b18752c1fe1db3ba67f7e2860251f
Progameitor <javigarzas27@gmail.com>
2019-11-12 18:09:20 +0100

    todo menos el bonus

---
+++ module-2/lab-bash/exercices/script.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#EJ1
+#echo " Hello World"
+#EJ2
+#mkdir new_dir
+#EJ3
+#rmdir new_dir
+#EJ4
+#cp "../lorem/sed.txt" "../lorem-copy"
+#EJ5
+#cp "../lorem/at.txt" "../lorem-copy" ;cp "../lorem/lorem.txt" "../lorem-copy"
+#EJ6
+#cat "../lorem/sed.txt" 
+#EJ7
+#cat "../lorem/at.txt" ; "../lorem/lorem.txt"
+#EJ8
+#head -n3 ../lorem-copy/sed.txt
+#EJ9
+#tail -3  ../lorem-copy/sed.txt
+#EJ10
+#echo "Homo homini lupus" >> "../lorem-copy/sed.txt"
+#EJ11
+#tail -3  ../lorem-copy/sed.txt
+#EJ12
+#sed 's/et/ET/g' "../lorem-copy/at.txt"
+#EJ13
+# who -a
+#EJ14
+#pwd
+#EJ15
+#ls ../lorem/*txt
+#EJ16
+#wc ../lorem/sed.txt
+#cat -n ../lorem/sed.txt no entiendo la diferencia entre los 2 primero 8 segundo 9
+#EJ17
+#find "../"  -name lorem* -type f| wc -l
+#EJ18
+#grep -nr et "../lorem/at.txt"
+#EJ19
+#grep -nr et "../lorem/at.txt" no funciona
+#cat ../lorem/at.txt | grep -w et no funciona
+#grep -o "et" ../lorem/at.txt | wc -l
+#EJ20
+#grep -o "et" ../lorem-copy/at.txt | wc -l
+
+
