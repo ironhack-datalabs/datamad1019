13e813b71d93c94a7ffec2f42bb94c1cf94ed47a
mariaadradosr <mariaadradosr@gmail.com>
2019-11-12 18:55:32 +0100

    Resolución lab

---
+++ module-2/lab-bash/bonus.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+NAME="Maria"
+# echo $NAME
+
+# vi bonus.sh
+# chmod +x bonus.sh
+
+# mkdir $NAME
+# rmdir $NAME
+
+# for file in $(ls lorem)
+# do
+# fn=${file%.*}
+# echo ${fn} 'has' ${#fn} 'characters length'
+# done 
+
+# top
+
+# ps -e 
+
+# sysctl -n machdep.cpu.brand_string
+
+# tar -cf lorem-compressed.tar.gz lorem/ lorem-copy/
+
+# mkdir lorem-uncompressed
+# tar -xvf lorem-compressed.tar.gz 
---
+++ module-2/lab-bash/ejercicios.sh
@@ -0,0 +1,21 @@
+1) echo 'Hello world' 
+2) mkdir new_dir 
+3) rmdir new_dir 
+4) cp lorem/sed.txt lorem-copy/sed.txt 
+5) cp ./lorem/at.txt ./lorem-copy/at.txt; cp ./lorem/lorem.txt ./lorem-copy/lorem.txt 
+6) cat at.txt
+    cat at.txt
+7) head -n 3 lorem.txt
+8) tail -n 3 lorem.txt 
+9) echo "Homo homini lupus." >> sed.txt
+10) tail -n 3 sed.txt
+11) sed -i -e 's/et/ET/g' at.txt
+12) who
+13) pwd
+14) find . -name '*.txt'
+14) wc -l sed.txt
+15) find . -name "lorem"
+16) grep -o -i et at.txt | wc -l
+17) grep -o -i et lorem.txt | wc -l
+18) grep -o -i et lorem-copy/* | wc -l
+
