7ec1817e7a8bdf8e700cb59918adf4a7669deb45
Julio Porto <juliopdata@gmail.com>
2019-11-12 22:54:44 +0100

    terminado bonus

--- module-2/lab-bash/lab-bash.sh
+++ module-2/lab-bash/lab-bash.sh
@@ -1,66 +1,66 @@
 #!/bin/bash
 
-# Imprime en consola `Hello World`.
+* Imprime en consola `Hello World`.
 # echo "Hello World"
 
-# Crea un directorio nuevo llamado `new_dir`.
+* Crea un directorio nuevo llamado `new_dir`.
 # mkdir new_dir
 
-# Elimina ese directorio.
+* Elimina ese directorio.
 # rmdir new_dir
 
-# Copia el archivo `sed.txt` dentro de la carpeta lorem a la carpeta lorem-copy.
+* Copia el archivo `sed.txt` dentro de la carpeta lorem a la carpeta lorem-copy.
 # cp lorem/sed.txt lorem-copy/
 
-# Copia los otros dos archivos de la carpeta lorem a la carpeta lorem-copy en una sola línea mediante `;`. 
-# cp lorem/* lorem-copy/
+* Copia los otros dos archivos de la carpeta lorem a la carpeta lorem-copy en una sola línea mediante `;`. 
+# cp lorem/at.txt lorem-copy; cp lorem/lorem.txt lorem-copy
 
-# Muestra el contenido del archivo `sed.txt` dentro de la carpeta lorem.
+* Muestra el contenido del archivo `sed.txt` dentro de la carpeta lorem.
 # cat lorem/sed.txt
 
-# Muestra el contenido de los archivos `at.txt` y `lorem.txt` dentro de la carpeta lorem.
+* Muestra el contenido de los archivos `at.txt` y `lorem.txt` dentro de la carpeta lorem.
 # cat lorem/at.txt <(echo) lorem/lorem.txt  <(echo) 
 
-# Visualiza las primeras 3 líneas del archivo `sed.txt` dentro de la carpeta lorem-copy
+* Visualiza las primeras 3 líneas del archivo `sed.txt` dentro de la carpeta lorem-copy
 # head -3 lorem-copy/sed.txt
 
-# Visualiza las ultimas 3 líneas del archivo `sed.txt` dentro de la carpeta lorem-copy 
+* Visualiza las ultimas 3 líneas del archivo `sed.txt` dentro de la carpeta lorem-copy 
 # tail -3 lorem-copy/sed.txt
 
-# Añade `Homo homini lupus.` al final de archivo `sed.txt` dentro de la carpeta lorem-copy.
+* Añade `Homo homini lupus.` al final de archivo `sed.txt` dentro de la carpeta lorem-copy.
 # echo 'Homo homini lupus.' >> lorem-copy/sed.txt
 
-# Visualiza las últimas 3 líneas del archivo `sed.txt` dentro de la carpeta lorem-copy. Deberías ver ahora `Homo homini lupus.`. 
+* Visualiza las últimas 3 líneas del archivo `sed.txt` dentro de la carpeta lorem-copy. Deberías ver ahora `Homo homini lupus.`. 
 # sed '4,10!d' lorem-copy/sed.txt
 
-# Sustituye todas las apariciones de `et` por `ET` del archivo `at.txt` dentro de la carpeta lorem-copy. Deberás usar `sed`. 
+* Sustituye todas las apariciones de `et` por `ET` del archivo `at.txt` dentro de la carpeta lorem-copy. Deberás usar `sed`. 
 # sed -i 's/et/ET/g' lorem-copy/at.txt
 
-# Encuentra al usuario activo en el sistema.
+* Encuentra al usuario activo en el sistema.
 # w
 
-# Encuentra dónde estás en tu sistema de ficheros.
+* Encuentra dónde estás en tu sistema de ficheros.
 # pwd
 
-# Lista los archivos que terminan por `.txt` en la carpeta lorem.
+* Lista los archivos que terminan por `.txt` en la carpeta lorem.
 # find lorem/ -type f -name "*.txt" 
 
-# Cuenta el número de líneas que tiene el archivo `sed.txt` dentro de la carpeta lorem.
+* Cuenta el número de líneas que tiene el archivo `sed.txt` dentro de la carpeta lorem.
 # wc -l lorem/sed.txt
 
-# Cuenta el número de **archivos** que empiezan por `lorem` que están en este directorio y en directorios internos.
+* Cuenta el número de **archivos** que empiezan por `lorem` que están en este directorio y en directorios internos.
 # ls  -dq **lorem** | wc -l
 
-# Encuentra todas las apariciones de `et` en `at.txt` dentro de la carpeta lorem.
+* Encuentra todas las apariciones de `et` en `at.txt` dentro de la carpeta lorem.
 # grep -Rw 'et' lorem/at.txt
 
-# Cuenta el número de apariciones del string `et` en `at.txt` dentro de la carpeta lorem. 
+* Cuenta el número de apariciones del string `et` en `at.txt` dentro de la carpeta lorem. 
 # grep -o 'et' lorem/at.txt | wc -l
 
-#  Cuenta el número de apariciones del string `et` en todos los archivos del directorio lorem-copy.
+*  Cuenta el número de apariciones del string `et` en todos los archivos del directorio lorem-copy.
 # grep -o 'et'  lorem-copy/* | wc -l
 
-## Bonus
+ Bonus
 
 * Almacena en una variable `name` tu nombre.
 
@@ -74,19 +74,22 @@
 #mkdir $name
 
 * Elimina ese directorio.
-#rmdir $name 
+# rmdir $name 
 
 * Por cada archivo dentro de la carpeta `lorem` imprime el número de carácteres que tienen sus nombres.
 Intenta primero mostrar los archivos mediante un bucle for 
     1. Imprime los ficheros
+    # for i in $(ls lorem); do echo $i; done
     2. Imprime las longitudes de los nombres de los ficheros
+    # for i in $(ls lorem); do echo ${#i}; done
     3. Imprime outputs con la siguiente estructura: `lorem has 5 characters lenght`
+    # for i in $(ls lorem); do echo $i has ${#i} characters lenght; done
 
 * Muestra los procesos de forma jerárquica que se están ejecutando en tu ordenador:
-    #1. Usando el comando top o htop
-    #htop -t
-    #2. Usando el comando ps con argumentos
-    #ps -aef --forest
+    1. Usando el comando top o htop
+    # htop -t
+    2. Usando el comando ps con argumentos
+    # ps -aef --forest
 
 * Muestra información sobre tu procesador por pantalla
 # lscpu
@@ -95,6 +98,7 @@ Intenta primero mostrar los archivos mediante un bucle for
 # alias ll="ls -la"
 # alias p='pwd'
 # alias HW='echo "Hello World"'
+
 * Comprime las carpetas lorem y lorem-copy en un archivo llamado lorem-compressed.tar.gz
 # tar -czf lorem-compressed.tar.gz lorem-copy/* lorem/*
 
