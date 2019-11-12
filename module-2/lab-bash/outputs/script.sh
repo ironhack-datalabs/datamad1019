7fdc0613c0659c9b17b9b4f2c1814c66b1d28264
María <verdejosinmaria@gmail.com>
2019-11-12 20:44:57 +0100

    terminado

--- module-2/lab-bash/outputs/script.sh
+++ module-2/lab-bash/outputs/script.sh
@@ -1,72 +1,73 @@
 #!/bin/bash
 
 #Ej 1
-    #echo 'Hello World!'
+    echo 'Hello World!'
 
 # EJ 2
-    #mkdir new_dir
+    mkdir new_dir
 
 # EJ 3
 
-    #rmdir new_dir
+    rmdir new_dir
 
 # EJ 4
 
-    #cp ../lorem/sed.txt ../lorem-copy
+    cp ../lorem/sed.txt ../lorem-copy
 
 # EJ 5
-    #cp ../lorem/at.txt ../lorem-copy; cp ../lorem/lorem.txt ../lorem-copy
+    cp ../lorem/at.txt ../lorem-copy; cp ../lorem/lorem.txt ../lorem-copy
 
 # EJ 6
-    #cat ../lorem/sed.txt
+    cat ../lorem/sed.txt
 
 # EJ 7
-    #cat ../lorem/at.txt ; cat ../lorem/lorem.txt
+    cat ../lorem/at.txt ; cat ../lorem/lorem.txt
 
 # EJ 8
-    #head -n3 ../lorem-copy/sed.txt
+    head -n3 ../lorem-copy/sed.txt
 
 # EJ 9
-    #tail -n3 ../lorem-copy/sed.txt
+    tail -n3 ../lorem-copy/sed.txt
 
 # EJ 10
-    #tail -n 3 ../lorem-copy/sed.txt | echo ' Homo homini lupus.' 
+    tail -n 3 ../lorem-copy/sed.txt | echo ' Homo homini lupus.' 
 
 #EJ 11
-    #tail -n 0 ../lorem-copy/sed.txt
+    tail -n 0 ../lorem-copy/sed.txt
 
 # EJ 12
-    #sed  's/ et / ET /' ../lorem-copy/at.txt
+    sed  's/ et / ET /' ../lorem-copy/at.txt
 
 # EJ 13
-    #id -u   NO SE SI ES CORRECTO, ME SALE UN NUMERO.
+    id -u   #NO SE SI ES CORRECTO, ME SALE UN NUMERO.
 
 # EJ 14
-    #pwd
+    pwd
 
 # EJ 15
 
-    # find ../lorem -name "*.txt"
+    find ../lorem -name "*.txt"
 
 # EJ 16
 
-    #wc -l ../lorem/sed.txt
+    wc -l ../lorem/sed.txt
 
 
 # EJ 17
 
-    # find lorem . | wc -l
+    find lorem . | wc -l
 
 
 # EJ 18
 
-    #grep -n et ../lorem/at.txt
+    grep -n et ../lorem/at.txt
 
 # EJ 19
 
 
-    #cat ../lorem/at.txt | grep -wo et | wc -w
+    cat ../lorem/at.txt | grep -wo et | wc -w
 
 
 # EJ 20
-find . -type f -exec grep -o et {} \; | wc -l
+
+    grep -roh et ../lorem-copy/*.txt | wc -w
