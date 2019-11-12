b83c3689877cbd7791920261510f11716faac2eb
Clara <clapiniella@gmail.com>
2019-11-12 17:45:37 +0100

    primera parte

---
+++ module-2/lab-bash/exercices/script.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+##ej1
+#echo "Hello world"
+##ej2
+#mkdir new_dir
+##ej3
+#rmdir new_dir
+##ej4
+#cp "../lorem/sed.txt" "../lorem-copy"
+##ej5
+#cp "../lorem/lorem.txt" "../lorem-copy"; cp "../lorem/at.txt" "../lorem-copy"
+##ej6
+#cat "../lorem/sed.txt"
+##ej7
+#cat "../lorem/lorem.txt" "../lorem/at.txt" 
+#ej8
+#head -n 3 "../lorem-copy/sed.txt"
+#ej9
+#tail -n 3 "../lorem-copy/sed.txt"
+#ej10
+#echo "Homo homini lupus." >> "../lorem-copy/sed.txt"
+#ej11
+#tail -n 3 "../lorem-copy/sed.txt"
+#ej12
+#sed "s/et/ET/" "../lorem-copy/sed.txt" 
+#ej13
+#cat /etc/passwd
+#who
+#ej14
+#pwd
+#ej15
+#find "../lorem/'*.txt'"
+#ls ../lorem/*.txt
+#ej16
+#wc -l "../lorem/sed.txt"
+#ej17
+#find "../"  -name lorem* -type f | wc -l
+#ej18
+#grep 'et' "../lorem/at.txt"
+#ej19
+#grep -o 'et' "../lorem/at.txt" | wc -l
+#ej20
+#grep -o 'et' ../lorem-copy/* | wc -l
