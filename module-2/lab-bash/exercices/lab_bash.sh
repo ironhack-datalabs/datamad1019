99b2eaed028edf8e79b3f52b04fe377c3518af15
mariobru <mariobrubarrero@gmail.com>
2019-11-12 16:46:58 +0100

    Ejericio 19

---
+++ module-2/lab-bash/exercices/lab_bash.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# Ejercicio 1
+
+#echo "Hello World"
+
+# Ejercicio 2
+
+# mkdir new_dir
+
+# Ejercicio 3
+
+# rmdir new_dir
+
+# Ejercicio 4
+
+# cp ../lorem/sed.txt ../lorem-copy/sed.txt
+
+# Ejercicio 5
+
+# cp ../lorem/lorem.txt ../lorem-copy/lorem.txt ; cp ../lorem/at.txt ../lorem-copy/at.txt
+
+# Ejercicio 6
+
+# cat ../lorem/sed.txt
+
+# Ejercicio 7
+
+# cat ../lorem/at.txt ;  cat ../lorem/lorem.txt
+
+# Ejercicio 8
+
+# head -3 ../lorem-copy/sed.txt
+
+# Ejercicio 9
+
+# tail -3 ../lorem-copy/sed.txt
+
+# Ejercicio 10
+
+# echo "Homo homini lupus." >> ../lorem-copy/sed.txt
+
+# Ejercicio 11
+
+# sed -i 's/et/ET/g' ../lorem-copy/at.txt
+
+# Ejercicio 12
+
+# whoami
+
+# Ejercicio 13
+
+# pwd
+
+# Ejercicio 14
+
+# ls -l ../lorem/*.txt
+
+# Ejercicio 15
+
+# cat ../lorem/lorem.txt | wc -l
+
+# Ejercicio 16
+
+# find ../ -name "lorem*" | wc -l
+
+# Ejercicio 17
+
+grep -o 'et' ../lorem/at.txt | wc -l
