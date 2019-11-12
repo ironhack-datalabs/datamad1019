662d02bdc413738c4cb3749b2d937e014524873b
albertovpd <a.vargas.pina@gmail.com>
2019-11-12 16:43:42 +0100

    ex 5 done

---
+++ module-2/lab-bash/exercises/script.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+# ex1: print "hello world"  (using in therminal the command cat+filename)
+#echo $"Hello World"
+
+# ex2: create a new directory 
+#mkdir new_dir in terminal
+
+# ex3: remove new directory
+# rm -d new_dir
+
+# ex4 Copia el archivo `sed.txt` dentro de la carpeta lorem a la carpeta lorem-copy. 
+# cp ../lorem/sed.txt ../lorem-copy/    
+
+#ex5 Copia los otros dos archivos de la carpeta lorem a la carpeta lorem-copy en una sola línea mediante `;`. 
+'''
+No aparece en el help el ";" . Buscar mejor luego
+'''
+# cp ../lorem/* ../lorem-copy/
+
+
+# ex6 Muestra el contenido del archivo `sed.txt` dentro de la carpeta lorem.
+
+
+#* Muestra el contenido de los archivos `at.txt` y `lorem.txt` dentro de la carpeta lorem. 
+
+#* Visualiza las primeras 3 líneas del archivo `sed.txt` dentro de la carpeta lorem-copy 
+
+#* Visualiza las ultimas 3 líneas del archivo `sed.txt` dentro de la carpeta lorem-copy 
+
+#* Añade `Homo homini lupus.` al final de archivo `sed.txt` dentro de la carpeta lorem-copy. 
+
+#* Visualiza las últimas 3 líneas del archivo `sed.txt` dentro de la carpeta lorem-copy. Deberías ver ahora `Homo homini lupus.`. 
+
+#* Sustituye todas las apariciones de `et` por `ET` del archivo `at.txt` dentro de la carpeta lorem-copy. Deberás usar `sed`. 
+
+#* Encuentra al usuario activo en el sistema.
+
+#* Encuentra dónde estás en tu sistema de ficheros.
+
+#* Lista los archivos que terminan por `.txt` en la carpeta lorem.
+
+#* Cuenta el número de líneas que tiene el archivo `sed.txt` dentro de la carpeta lorem. 
+
+#* Cuenta el número de **archivos** que empiezan por `lorem` que están en este directorio y en directorios internos.
+
+#* Encuentra todas las apariciones de `et` en `at.txt` dentro de la carpeta lorem.
+
+#* Cuenta el número de apariciones del string `et` en `at.txt` dentro de la carpeta lorem. 
+
+#*  Cuenta el número de apariciones del string `et` en todos los archivos del directorio lorem-copy. 
+
+## Ficheros bash
+
+#Manual vi: https://www.howtogeek.com/102468/a-beginners-guide-to-editing-text-files-with-vi/
+
+#Cualquier comando o comandos de bash se pueden almacenar en un fichero y ejecutar cuando queramos. 
+#Obviamente puedes utilizar tu editor preferido. Creamos el fichero: 
+```
+$ vi list_files.sh
+```
+
+#E incluimos el contenido que queramos. En este caso listar ficheros:
+```bash
+!/bin/bash
+ls
+```
+
+#Ejecutamos el script:
+```
+$ bash list_files.sh
+```
+
+#Y veremos por consola el siguiente output. 
+```console
+exercices  inputs  lorem  lorem-copy  modules  outputs  README.md
+```
+
+## Bonus
+
+#* Almacena en una variable `name` tu nombre.
+
+#* Imprime esa variable.
+
+#* Crea un directorio nuevo que se llame como el contenido de la variable `name`.
+
+#* Elimina ese directorio. 
+
+#* Por cada archivo dentro de la carpeta `lorem` imprime el número de carácteres que tienen sus nombres. 
+#Intenta primero mostrar los archivos mediante un bucle for 
+#    1. Imprime los ficheros
+#    2. Imprime las longitudes de los nombres de los ficheros
+#    3. Imprime outputs con la siguiente estructura: `lorem has 5 characters lenght`
+
+#* Muestra los procesos de forma jerárquica que se están ejecutando en tu ordenador:
+#    1. Usando el comando top o htop
+#    2. Usando el comando ps con argumentos
+
+#* Muestra información sobre tu procesador por pantalla
+
+#* Crea 3 alias y haz que estén disponibles cada vez que inicias sesión
+
+#* Comprime las carpetas lorem y lorem-copy en un archivo llamado lorem-compressed.tar.gz
+
+#* Descomprime el archivo lorem-compressed.tar.gz en la carpeta lorem-uncompressed
