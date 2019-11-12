cd34a161f72af53f1b6e20964a85316c3dccc53c
Antonio Pons <toninopons@gmail.com>
2019-11-12 18:59:31 +0100

    lab listo falta bonus

---
+++ module-2/lab-bash/exercices.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#Imprime en consola `Hello World`.
+
+#echo "Hello World"
+
+#Crea un directorio nuevo llamado `new_dir`.
+
+#mkdir "new_dir"
+
+#Elimina ese directorio.
+
+#rm -r "new_dir"
+
+#Copia el archivo `sed.txt` dentro de la carpeta lorem a la carpeta lorem-copy.
+
+#cp ./lorem/sed.txt ./lorem-copy
+
+#Copia los otros dos archivos de la carpeta lorem a la carpeta lorem-copy 
+#en una sola línea mediante `;`.
+
+#cp ./lorem/at.txt ./lorem-copy; cp ./lorem/lorem.txt ./lorem-copy
+
+#Muestra el contenido del archivo `sed.txt` dentro de la carpeta lorem.
+#cat ./lorem/sed.txt
+
+#Muestra el contenido de los archivos `at.txt` y `lorem.txt` 
+#dentro de la carpeta lorem. 
+
+#cat ./lorem/at.txt; cat ./lorem/lorem.txt
+
+#Visualiza las primeras 3 líneas del archivo `sed.txt` dentro de la carpeta lorem-copy 
+
+#head -3 ./lorem/sed.txt
+
+#Visualiza las ultimas 3 líneas del archivo `sed.txt` dentro de la carpeta lorem-copy 
+
+#tail -3 ./lorem/sed.txt
+
+#Añade `Homo homini lupus.` al final de archivo `sed.txt` 
+#dentro de la carpeta lorem-copy. 
+
+#echo "Homo homini lupus." >> ./lorem-copy/sed.txt
+
+#Visualiza las últimas 3 líneas del archivo `sed.txt` 
+#dentro de la carpeta lorem-copy. Deberías ver ahora `Homo homini lupus.`. 
+
+#tail -3 ./lorem-copy/sed.txt
+
+#Sustituye todas las apariciones de `et` por `ET` del archivo `at.txt` 
+#dentro de la carpeta lorem-copy. Deberás usar `sed`. 
+
+#sed 's/et/ET/' ./lorem-copy/at.txt
+
+#Encuentra al usuario activo en el sistema.
+
+#whoami
+
+* Encuentra dónde estás en tu sistema de ficheros.
+
