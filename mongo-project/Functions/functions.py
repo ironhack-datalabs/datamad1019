bf6a04026700dbb54d5a70eb1398205a9e5b7499
Julio Porto <juliopdata@gmail.com>
2019-11-21 12:12:09 +0100

    probando con Chicago

--- mongo-project/Functions/functions.py
+++ mongo-project/Functions/functions.py
@@ -50,3 +50,15 @@ def moneyRaise(value):
         exchange = value_number
     return int(exchange)
 
+def findNear(list_geo, radio_meters):
+        geopoint = list_geo
+        return list(db.data_companies_clean.find({
+        "geoDescription": {
+         "$near": {
+             "$geometry": geopoint,
+             "$maxDistance": radio_meters
+         }
+       }
+    }
+    )
+    ) 
